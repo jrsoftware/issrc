8213db748a48946043a91cfdfe6aff24eb0f154f
Martijn Laan <martijnlaan@gmail.com>
2012-05-09 17:20:44 +0200

    Updated for new path.

--- Projects/ISPP/synch-isfiles.bat
+++ Projects/ISPP/synch-isfiles.bat
@@ -1,12 +1,12 @@
-copy ..\is\ishelp\isetup.dtd help
-copy ..\is\ishelp\staging\contents.css help\staging
-copy ..\is\ishelp\staging\contents.js help\staging
-copy ..\is\ishelp\staging\styles.css help\staging
-copy ..\is\ishelp\staging\topic.js help\staging
-copy ..\is\ishelp\staging\images\contentsheadclosed.gif help\staging\images
-copy ..\is\ishelp\staging\images\contentsheadopen.gif help\staging\images
-copy ..\is\ishelp\staging\images\contentstopic.gif help\staging\images
-copy ..\is\ishelp\staging\images\extlink.gif help\staging\images
+copy ..\..\ishelp\isetup.dtd help
+copy ..\..\ishelp\staging\contents.css help\staging
+copy ..\..\ishelp\staging\contents.js help\staging
+copy ..\..\ishelp\staging\styles.css help\staging
+copy ..\..\ishelp\staging\topic.js help\staging
+copy ..\..\ishelp\staging\images\contentsheadclosed.gif help\staging\images
+copy ..\..\ishelp\staging\images\contentsheadopen.gif help\staging\images
+copy ..\..\ishelp\staging\images\contentstopic.gif help\staging\images
+copy ..\..\ishelp\staging\images\extlink.gif help\staging\images
 
 
 pause
