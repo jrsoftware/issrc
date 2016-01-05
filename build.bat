b8d66bad8072bc620104c78294ea33cdac23a08d
Martijn Laan <martijnlaan@gmail.com>
2016-01-05 10:09:05 +0100

    Use /qp (was using already from my own setup-sign.bat).

--- build.bat
+++ build.bat
@@ -54,7 +54,7 @@ echo - Setup.exe
 if exist .\setup-sign.bat (
   call .\setup-sign.bat
 ) else (
-  files\iscc setup.iss /q /DNOSIGNTOOL
+  files\iscc setup.iss /qp /DNOSIGNTOOL
 )
 if errorlevel 1 goto failed
 echo - Renaming files
