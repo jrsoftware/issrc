cd1e925a107d034bfec443fe234904fab8b3f134
Martijn Laan <martijnlaan@gmail.com>
2016-02-05 13:38:47 +0100

    File name case fixes.

--- Examples/MyDll/C#/Properties/AssemblyInfo.cs
+++ Examples/MyDll/C#/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mydll")]
+[assembly: AssemblyTitle("MyDll")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mydll")]
+[assembly: AssemblyProduct("MyDll")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
