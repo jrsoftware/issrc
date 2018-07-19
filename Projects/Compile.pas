e5894f71f3d97c27c24c961fd9f3c8dec80b124b
jogo- <jogo@laposte.net>
2018-07-19 05:43:35 +0200

    Update Compile.pas
    Fix typo in 'ChangesEnviroment'

--- Projects/Compile.pas
+++ Projects/Compile.pas
@@ -8702,7 +8702,7 @@ begin
     LineNumber := SetupDirectiveLines[ssUninstallable];
     CheckCheckOrInstall('Uninstallable', SetupHeader.Uninstallable, cikDirectiveCheck);
     LineNumber := SetupDirectiveLines[ssChangesEnvironment];
-    CheckCheckOrInstall('ChangesEnviroment', SetupHeader.ChangesEnvironment, cikDirectiveCheck);
+    CheckCheckOrInstall('ChangesEnvironment', SetupHeader.ChangesEnvironment, cikDirectiveCheck);
     LineNumber := SetupDirectiveLines[ssChangesAssociations];
     CheckCheckOrInstall('ChangesAssociations', SetupHeader.ChangesAssociations, cikDirectiveCheck);
     if Output then begin
