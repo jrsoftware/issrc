996d896749ad55de4c0c9eebd4658bafcf8d82ab
jogo- <jogo@laposte.net>
2020-08-10 15:58:10 +0200

    Fix typo in CompForm.pas

--- Projects/CompForm.pas
+++ Projects/CompForm.pas
@@ -723,7 +723,7 @@ begin
   Call(SCI_SETSCROLLWIDTH, 1024 * CallStr(SCI_TEXTWIDTH, 0, 'X'), 0);
 
   Call(SCI_INDICSETSTYLE, inSquiggly, INDIC_SQUIGGLE);
-  Call(SCI_INDICSETFORE, inSquiggly, clRed); { May be overwitten by UpdateThemeColors }
+  Call(SCI_INDICSETFORE, inSquiggly, clRed); { May be overwritten by UpdateThemeColors }
   Call(SCI_INDICSETSTYLE, inPendingSquiggly, INDIC_HIDDEN);
 
   Call(SCI_SETMARGINTYPEN, 1, SC_MARGIN_SYMBOL);
