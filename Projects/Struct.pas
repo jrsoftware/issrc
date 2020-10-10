ad5630f1731d9d6ca4590c2580434653c16ab5fa
Martijn Laan <martijnlaan@gmail.com>
2020-10-10 22:00:52 +0200

    Bump version.

--- Projects/Struct.pas
+++ Projects/Struct.pas
@@ -17,8 +17,8 @@ uses
 
 const
   SetupTitle = 'Inno Setup';
-  SetupVersion = '6.1.0-beta';
-  SetupBinVersion = (6 shl 24) + (1 shl 16) + (0 shl 8) + 0;
+  SetupVersion = '6.1.1-beta';
+  SetupBinVersion = (6 shl 24) + (1 shl 16) + (1 shl 8) + 0;
 
 type
   TSetupID = array[0..63] of AnsiChar;
