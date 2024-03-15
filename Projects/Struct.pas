5a0629d3de60a03e720c7382a080869a972e907d
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-03-15 08:23:02 +0100

    Bump version. Does not mean a release is imminent.

--- Projects/Struct.pas
+++ Projects/Struct.pas
@@ -17,8 +17,8 @@ uses
 
 const
   SetupTitle = 'Inno Setup';
-  SetupVersion = '6.2.3-dev';
-  SetupBinVersion = (6 shl 24) + (2 shl 16) + (3 shl 8) + 0;
+  SetupVersion = '6.3.0';
+  SetupBinVersion = (6 shl 24) + (3 shl 16) + (0 shl 8) + 0;
 
 type
   TSetupID = array[0..63] of AnsiChar;
