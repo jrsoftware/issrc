c46cb592c650f04348723598f39c6dd563c38795
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-06-09 13:30:02 +0200

    Add comment.

--- Projects/Src/CompForm.pas
+++ Projects/Src/CompForm.pas
@@ -3259,7 +3259,8 @@ procedure TCompileForm.UpdateOccurrenceIndicators(const AMemo: TCompScintEdit);
       if Markers * [mmLineError, mmLineBreakpointBad, mmLineStep] <> [] then
         Continue;
 
-      { Add indicator while making sure it does not overlap the regular selection styling }
+      { Add indicator while making sure it does not overlap the regular selection
+       styling (only looks at main selection and not any additional selections atm) }
       if SelAvail and Range.Overlaps(Selection) then begin
         if Range.StartPos < Selection.StartPos then
           ARangeList.Add(TScintRange.Create(Range.StartPos, Selection.StartPos));
