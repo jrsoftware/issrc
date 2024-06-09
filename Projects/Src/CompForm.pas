666a96ca6e7dbbfb55aac5be2239cc4ddfc340a6
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-06-09 19:08:06 +0200

    Extend comment.

--- Projects/Src/CompForm.pas
+++ Projects/Src/CompForm.pas
@@ -3260,7 +3260,10 @@ procedure TCompileForm.UpdateOccurrenceIndicators(const AMemo: TCompScintEdit);
         Continue;
 
       { Add indicator while making sure it does not overlap the regular selection
-       styling (only looks at main selection and not any additional selections atm) }
+        styling (only looks at main selection and not any additional selections
+        atm - so if you ctrl drag to select a part of a word and then the same
+        on an occurrence somewhere else the additional selection becomes hidden
+        by the indicator except for the very top and bottom }
       if SelAvail and Range.Overlaps(Selection) then begin
         if Range.StartPos < Selection.StartPos then
           ARangeList.Add(TScintRange.Create(Range.StartPos, Selection.StartPos));
