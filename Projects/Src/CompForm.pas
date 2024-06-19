58326f1e8825f4bdb59e852a0c609b9c71619c6f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-06-19 07:44:53 +0200

    Tweak/fix for SCI_VCHOMERECTEXTEND and others.

--- Projects/Src/CompForm.pas
+++ Projects/Src/CompForm.pas
@@ -2853,7 +2853,9 @@ end;
 procedure TCompileForm.MemoKeyDown(Sender: TObject; var Key: Word;
   Shift: TShiftState);
 begin
-  if Key in [VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN, VK_HOME, VK_END] then begin
+  if (Key in [VK_LEFT, VK_RIGHT, VK_UP, VK_DOWN, VK_HOME, VK_END]) and
+     { Versions with Shift+Alt are special rectangular select shortcuts so don't break those }
+     not (Shift * [ssShift, ssAlt] = [ssShift, ssAlt]) then begin
     var Memo := Sender as TScintEdit;
     if Memo.SelectionMode in [ssmRectangular, ssmThinRectangular] then begin
        { Allow left/right/etc. navigation with rectangular selection, see
