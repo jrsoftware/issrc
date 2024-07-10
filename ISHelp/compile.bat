0542ad66fa8d9afbecdd9118b9192262fc9b20dc
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-07-10 20:20:04 +0200

    Cleanup/typos.

--- ISHelp/compile.bat
+++ ISHelp/compile.bat
@@ -11,7 +11,7 @@ setlocal
 
 if exist compilesettings.bat goto compilesettingsfound
 :compilesettingserror
-echo ishelp\compilesettings.bat is missing or incomplete. It needs to be created
+echo ISHelp\compilesettings.bat is missing or incomplete. It needs to be created
 echo with the following lines, adjusted for your system:
 echo.
 echo   set HHCEXE=%%ProgramFiles%%\HTML Help Workshop\hhc.exe   [Path to help compiler]
