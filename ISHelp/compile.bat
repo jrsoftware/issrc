f2eed0143670f5ca48ec43181098fafba5835d43
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-07-20 08:50:10 +0200

    Fix typo and whitespace.

--- ISHelp/compile.bat
+++ ISHelp/compile.bat
@@ -29,7 +29,7 @@ rem -------------------------------------------------------------------------
 echo Synching dark files:
 echo.
 call synch-darkfiles.bat nopause
-if errorlevel 1 goto failed 
+if errorlevel 1 goto failed
 
 call :generate_help
 if errorlevel 1 goto failed
