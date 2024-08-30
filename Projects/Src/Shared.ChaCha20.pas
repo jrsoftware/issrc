51486a1813c01c32e8f4ff5bd015f927bbc2431f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2024-08-30 20:34:35 +0200

    Make it like IETF ChaCha by removing the overlap of the counter and nonce. Also some cleanup/renames.

--- Projects/Src/Shared.ChaCha20.pas
+++ Projects/Src/Shared.ChaCha20.pas
@@ -22,7 +22,7 @@ type
   TChaChaCtx = array[0..15] of Cardinal;
 
   TChaChaContext = record
-    ctx: TChaChaCtx;
+    ctx, keystream: TChaChaCtx;
   end;
 
 procedure ChaCha20Init(var Context: TChaChaContext; const Key;
@@ -73,41 +73,36 @@ procedure ChaCha20Crypt(var Context: TChaChaContext; const InBuffer;
     Inc(c, d); b := b xor c; b := ROTL(b, 7);
   end;
 
-  procedure ChaCha20BlockNext(const input: TChaChaCtx; var output: TChaChaCtx);
+  procedure ChaCha20BlockNext(var ctx, keystream: TChaChaCtx);
   begin
     for var i := 0 to 15 do
-      output[i] := input[i];
+      keystream[i] := ctx[i];
 
     for var i := 0 to 9 do begin
-      CHACHA20_QR(output[0], output[4], output[8], output[12]);  // column 0
-      CHACHA20_QR(output[1], output[5], output[9], output[13]);  // column 1
-      CHACHA20_QR(output[2], output[6], output[10], output[14]); // column 2
-      CHACHA20_QR(output[3], output[7], output[11], output[15]); // column 3
-      CHACHA20_QR(output[0], output[5], output[10], output[15]); // diagonal 1 (main diagonal)
-      CHACHA20_QR(output[1], output[6], output[11], output[12]); // diagonal 2
-      CHACHA20_QR(output[2], output[7], output[8], output[13]);  // diagonal 3
-      CHACHA20_QR(output[3], output[4], output[9], output[14]);  // diagonal 4
+      CHACHA20_QR(keystream[0], keystream[4], keystream[8], keystream[12]);  // column 0
+      CHACHA20_QR(keystream[1], keystream[5], keystream[9], keystream[13]);  // column 1
+      CHACHA20_QR(keystream[2], keystream[6], keystream[10], keystream[14]); // column 2
+      CHACHA20_QR(keystream[3], keystream[7], keystream[11], keystream[15]); // column 3
+      CHACHA20_QR(keystream[0], keystream[5], keystream[10], keystream[15]); // diagonal 1 (main diagonal)
+      CHACHA20_QR(keystream[1], keystream[6], keystream[11], keystream[12]); // diagonal 2
+      CHACHA20_QR(keystream[2], keystream[7], keystream[8], keystream[13]);  // diagonal 3
+      CHACHA20_QR(keystream[3], keystream[4], keystream[9], keystream[14]);  // diagonal 4
     end;
 
     for var i := 0 to 15 do
-      output[i] := output[i] + input[i];
+      keystream[i] := keystream[i] + ctx[i];
+
+    Assert(ctx[12] < High(Cardinal));
+    ctx[12] := ctx[12] + 1;
   end;
 
 begin
   var InBuf: PByte := @InBuffer;
   var OutBuf: PByte := @OutBuffer;
   while Length > 0 do begin
-    var Tmp: TChaChaCtx;
-    ChaCha20BlockNext(Context.ctx, Tmp);
-    if Context.ctx[12] < High(Cardinal) then
-      Context.ctx[12] := Context.ctx[12] + 1
-    else begin
-      Context.ctx[12] := 0;
-      Assert(Context.ctx[13] < High(Cardinal));
-      Context.ctx[13] := Context.ctx[13] + 1;
-    end;
+    ChaCha20BlockNext(Context.ctx, Context.keystream);
 
-    var KeyStream: PByte := @Tmp;
+    var KeyStream: PByte := @Context.keystream;
     var BlockSize := Min(Length, 64);
     for var I := 0 to  BlockSize - 1 do
       OutBuf[I] := InBuf[I] xor KeyStream[i];
