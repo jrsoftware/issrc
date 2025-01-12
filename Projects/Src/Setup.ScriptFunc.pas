fdf652b434776712ce0a236493e6b46a3007c7dc
Jordan Russell <jr-github@jrsoftware.org>
2025-01-12 02:29:33 -0600

    Set PopupMode=pmAuto in CreateCustomForm support function.
    Only really matters when a custom form displays another custom form.

--- Projects/Src/Setup.ScriptFunc.pas
+++ Projects/Src/Setup.ScriptFunc.pas
@@ -2,7 +2,7 @@ unit Setup.ScriptFunc;
 
 {
   Inno Setup
-  Copyright (C) 1997-2024 Jordan Russell
+  Copyright (C) 1997-2025 Jordan Russell
   Portions by Martijn Laan
   For conditions of distribution and use, see LICENSE.TXT.
 
@@ -291,6 +291,7 @@ var
     begin
       var NewSetupForm := TSetupForm.CreateNew(nil);
       try
+        NewSetupForm.PopupMode := pmAuto;
         NewSetupForm.AutoScroll := False;
         NewSetupForm.BorderStyle := bsDialog;
         NewSetupForm.InitializeFont;
