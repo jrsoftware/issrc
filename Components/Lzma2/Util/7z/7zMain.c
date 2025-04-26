jrsoftware/issrc

commit 1fd19b5d31c274b89128ad105062ee57c26f2d92

Support output/log of Unicode directory and file names.

Components/Lzma2/Util/7z/7zMain.c
@@ -2,9 +2,10 @@
 2024-02-28 : Igor Pavlov : Public domain */
 
 /* Changes by Martijn Laan for Inno Setup:
-   -Does not include <stdio.h> but instead defines fputs' prototype and a stdout dummy, allowing
+   -Don't include <stdio.h> but instead define fputs' prototype and a stdout dummy, allowing
     the test application code to be embedded in another application (which should implement fputs)
-   Zero other changes */
+   -Use CP_UTF8 in PrintString
+   Otherwise unchanged */
 
 #include "Precomp.h"
 
@@ -261,7 +262,7 @@ static SRes PrintString(const UInt16 *s)
   Buf_Init(&buf);
   res = Utf16_To_Char(&buf, s
       #ifndef MY_USE_UTF8
-      , CP_OEMCP
+      , CP_UTF8
       #endif
       );
   if (res == SZ_OK)

Projects/Src/Compression.SevenZipDecoder.pas
@@ -67,7 +67,7 @@ end;
 
 function _fputs(str: PAnsiChar; unused: Pointer): Integer; cdecl;
 begin
-  Log(String(str));
+  Log(UTF8ToString(str));
   Result := 1;
 end;
 

commit d8a1a8f751bed516a67711c999a3156ea9bfd20c

Fix invalid WideCharToMultiByte parameters, caused by 1fd19b5d and probably causing an error on Windows 2016 at least.
Win32 docs for the two params: "For the CP_UTF7 and CP_UTF8 settings for CodePage, this parameter must be set to NULL. Otherwise, the function fails with ERROR_INVALID_PARAMETER."

Components/Lzma2/Util/7z/7zMain.c
@@ -194,8 +194,10 @@ static SRes Utf16_To_Char(CBuf *buf, const UInt16 *s
       {
         const char defaultChar = '_';
         BOOL defUsed;
+        const char *pDefaultChar = (codePage == CP_UTF7 || codePage == CP_UTF8) ? NULL : &defaultChar;
+        BOOL *pDefUsed = pDefaultChar ? NULL : &defUsed;
         const unsigned numChars = (unsigned)WideCharToMultiByte(
-            codePage, 0, (LPCWSTR)s, (int)len, (char *)buf->data, (int)size, &defaultChar, &defUsed);
+            codePage, 0, (LPCWSTR)s, (int)len, (char *)buf->data, (int)size, pDefaultChar, pDefUsed);
         if (numChars == 0 || numChars >= size)
           return SZ_ERROR_FAIL;
         buf->data[numChars] = 0;