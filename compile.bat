18d4074d6ec4149cf3e54a9db3a915954606a19f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-06-12 13:40:12 +0200

    Increase match with the .dprojs and what the IDE shows as their dcc32 commandline.
    Also add missing -H to Setup.dpr and use some locals to avoid duplication.
    
    -$L-    : Local symbol information off
    -CC     : Console target
    -TX.e32 : Output name extension .e32
    -H      : Output hint messages

--- compile.bat
+++ compile.bat
@@ -24,6 +24,8 @@ set DELPHIXEROOT=
 call .\compilesettings.bat
 if "%DELPHIXEROOT%"=="" goto compilesettingserror
 
+set DELPHIXELIB=%DELPHIXEROOT%\lib\win32\release
+
 rem -------------------------------------------------------------------------
 
 rem  Compile each project separately because it seems Delphi
@@ -31,6 +33,14 @@ rem  carries some settings (e.g. $APPTYPE) between projects
 rem  if multiple projects are specified on the command line.
 
 set DELPHIXEDISABLEDWARNINGS=-W-SYMBOL_DEPRECATED -W-SYMBOL_PLATFORM -W-UNSAFE_CAST -W-EXPLICIT_STRING_CAST -W-EXPLICIT_STRING_CAST_LOSS -W-IMPLICIT_INTEGER_CAST_LOSS -W-IMPLICIT_CONVERSION_LOSS
+
+set STANDARDFLAGS=--no-config -Q -B -$L- -$C- -H -W %DELPHIXEDISABLEDWARNINGS% %1 -E..\Files
+set STANDARDFLAGSCONSOLE=%STANDARDFLAGS% -CC
+set STANDARDFLAGSE32=%STANDARDFLAGS% -TX.e32
+set STANDARDNAMESPACES=System;System.Win;Winapi
+
+set ROPSSRC=..\Components\UniPS\Source
+set ROPSDEF=PS_MINIVCL;PS_NOGRAPHCONST;PS_PANSICHAR;PS_NOINTERFACEGUIDBRACKETS
 
 cd Projects
 if errorlevel 1 goto failed
@@ -40,48 +50,38 @@ if not "%1"=="" goto failed
 
 echo - ISPP.dpr
 mkdir Dcu\ISPP.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" --no-config -NSSystem;System.Win;Winapi -Q -B -H -W %DELPHIXEDISABLEDWARNINGS% %1 -U"%DELPHIXEROOT%\lib\win32\release" -E..\Files -NUDcu\ISPP.dpr ISPP.dpr
+"%DELPHIXEROOT%\bin\dcc32.exe" %STANDARDFLAGSCONSOLE% -NS%STANDARDNAMESPACES%  -U"%DELPHIXELIB%"  -NUDcu\ISPP.dpr ISPP.dpr
 if errorlevel 1 goto failed
 
 echo - Compil32.dpr
 mkdir Dcu\Compil32.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" --no-config -NSSystem;System.Win;Winapi;Vcl;Vcl.Imaging -Q -B -H -W %DELPHIXEDISABLEDWARNINGS% %1 -U"%DELPHIXEROOT%\lib\win32\release;..\Components\UniPs\Source" -E..\Files -NUDcu\Compil32.dpr -DCOMPIL32PROJ;PS_MINIVCL;PS_NOGRAPHCONST;PS_PANSICHAR;PS_NOINTERFACEGUIDBRACKETS Compil32.dpr
+"%DELPHIXEROOT%\bin\dcc32.exe" %STANDARDFLAGS% -NS%STANDARDNAMESPACES%;Vcl;Vcl.Imaging -U"%DELPHIXELIB%;%ROPSSRC%" -NUDcu\Compil32.dpr -DCOMPIL32PROJ;%ROPSDEF% Compil32.dpr
 if errorlevel 1 goto failed
 
 echo - ISCC.dpr
 mkdir Dcu\ISCC.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" --no-config -NSSystem;System.Win;Winapi -Q -B -H -W %DELPHIXEDISABLEDWARNINGS% %1 -U"%DELPHIXEROOT%\lib\win32\release;..\Components\UniPs\Source" -E..\Files -NUDcu\ISCC.dpr -DPS_MINIVCL;PS_NOGRAPHCONST;PS_PANSICHAR;PS_NOINTERFACEGUIDBRACKETS ISCC.dpr
+"%DELPHIXEROOT%\bin\dcc32.exe" %STANDARDFLAGS% -NS%STANDARDNAMESPACES% -U"%DELPHIXELIB%;%ROPSSRC%" -NUDcu\ISCC.dpr -D%ROPSDEF% ISCC.dpr
 if errorlevel 1 goto failed
 
 echo - ISCmplr.dpr
 mkdir Dcu\ISCmplr.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" --no-config -NSSystem;System.Win;Winapi -Q -B -H -W %DELPHIXEDISABLEDWARNINGS% %1 -U"%DELPHIXEROOT%\lib\win32\release;..\Components\UniPs\Source" -E..\Files -NUDcu\ISCmplr.dpr -DPS_MINIVCL;PS_NOGRAPHCONST;PS_PANSICHAR;PS_NOINTERFACEGUIDBRACKETS ISCmplr.dpr
+"%DELPHIXEROOT%\bin\dcc32.exe" %STANDARDFLAGS% -NS%STANDARDNAMESPACES% -U"%DELPHIXELIB%;%ROPSSRC%" -NUDcu\ISCmplr.dpr -D%ROPSDEF% ISCmplr.dpr
 if errorlevel 1 goto failed
 
 echo - SetupLdr.dpr
 mkdir Dcu\SetupLdr.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" --no-config -NSSystem;System.Win;winapi -Q -B -H -W %DELPHIXEDISABLEDWARNINGS% %1 -U"%DELPHIXEROOT%\lib\win32\release" -E..\Files -NUDcu\SetupLdr.dpr -DSETUPLDRPROJ SetupLdr.dpr
+"%DELPHIXEROOT%\bin\dcc32.exe" %STANDARDFLAGSE32% -NS%STANDARDNAMESPACES% -U"%DELPHIXELIB%" -NUDcu\SetupLdr.dpr -DSETUPLDRPROJ SetupLdr.dpr
 if errorlevel 1 goto failed
 
 echo - Setup.dpr
 mkdir Dcu\Setup.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" --no-config -NSSystem;System.Win;Winapi;Vcl -Q -B -W %DELPHIXEDISABLEDWARNINGS% %1 -U"%DELPHIXEROOT%\lib\win32\release;..\Components\UniPs\Source" -E..\Files -NUDcu\Setup.dpr -DSETUPPROJ;PS_MINIVCL;PS_NOGRAPHCONST;PS_PANSICHAR;PS_NOINTERFACEGUIDBRACKETS Setup.dpr
-if errorlevel 1 goto failed
-
-echo - Renaming E32 files
-cd ..\Files
-if errorlevel 1 goto failed
-move SetupLdr.exe SetupLdr.e32
-if errorlevel 1 goto failed
-move Setup.exe Setup.e32
-if errorlevel 1 goto failed
-cd ..\Projects
+"%DELPHIXEROOT%\bin\dcc32.exe" %STANDARDFLAGSE32% -NS%STANDARDNAMESPACES%;Vcl -U"%DELPHIXELIB%;%ROPSSRC%" -NUDcu\Setup.dpr -DSETUPPROJ;%ROPSDEF% Setup.dpr
 if errorlevel 1 goto failed
 
 :issigtool
 echo - ISSigTool.dpr
 mkdir Dcu\ISSigTool.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" --no-config -NSSystem;System.Win;Winapi -Q -B -H -W %DELPHIXEDISABLEDWARNINGS% %1 -U"%DELPHIXEROOT%\lib\win32\release" -E..\Files -NUDcu\ISSigTool.dpr ISSigTool.dpr
+"%DELPHIXEROOT%\bin\dcc32.exe" %STANDARDFLAGSCONSOLE% -NS%STANDARDNAMESPACES% -U"%DELPHIXELIB%" -NUDcu\ISSigTool.dpr ISSigTool.dpr
 if errorlevel 1 goto failed
 
 cd ..
