7ce7187a5c7ab06293a27b9c8a16c480587ca278
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-06-22 19:15:23 +0200

    Add -CC like was done in the main compile.bat.

--- ISHelp/ISHelpGen/compile.bat
+++ ISHelp/ISHelpGen/compile.bat
@@ -29,12 +29,13 @@ rem -------------------------------------------------------------------------
 set DELPHIXEDISABLEDWARNINGS=-W-SYMBOL_DEPRECATED -W-SYMBOL_PLATFORM -W-UNSAFE_CAST -W-EXPLICIT_STRING_CAST -W-EXPLICIT_STRING_CAST_LOSS -W-IMPLICIT_INTEGER_CAST_LOSS -W-IMPLICIT_CONVERSION_LOSS
 
 set FLAGS=--no-config -Q -B -$L- -$C- -H -W %DELPHIXEDISABLEDWARNINGS% %1
+set FLAGSCONSOLE=%FLAGS% -CC
 set NAMESPACES=System;System.Win;Winapi
 set DCUDIR=Dcu\Release
 
 echo Compiling ISHelpGen.dpr:
 mkdir %DCUDIR%\ISHelpGen.dpr 2>nul
-"%DELPHIXEROOT%\bin\dcc32.exe" %FLAGS% -NS%NAMESPACES% -U"%DELPHIXELIB%" -NU%DCUDIR%\ISHelpGen.dpr ISHelpGen.dpr
+"%DELPHIXEROOT%\bin\dcc32.exe" %FLAGSCONSOLE% -NS%NAMESPACES% -U"%DELPHIXELIB%" -NU%DCUDIR%\ISHelpGen.dpr ISHelpGen.dpr
 if errorlevel 1 goto failed
 
 echo Success!
