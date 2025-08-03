3c3ca70f6e022b20341574b8fe5dbe2fa15772cc
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-08-03 19:25:40 +0200

    Call 'issig.bat sign' from compile.bat as well, to make the files in Files useable after using compile.bat instead of build.bat.

--- compile.bat
+++ compile.bat
@@ -89,6 +89,12 @@ cd ..
 if errorlevel 1 goto failed
 
 echo Success!
+
+rem  Sign using user's private key - will be overwritten if called by build.bat
+call .\issig.bat sign Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\SetupLdr.e32
+if errorlevel 1 goto failed
+echo ISSigTool sign done
+
 goto exit
 
 :failed
