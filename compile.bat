jrsoftware/issrc

commit 3c3ca70f6e022b20341574b8fe5dbe2fa15772cc

Call 'issig.bat sign' from compile.bat as well, to make the files in Files useable after using compile.bat instead of build.bat.

compile.bat
@@ -89,6 +89,12 @@ cd ..
 if errorlevel 1 goto failed
 
 echo Success!
+
+rem  Sign using user's private key - will be overwritten if called by build.bat
+call .\issig.bat sign Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\SetupLdr.e32
+if errorlevel 1 goto failed
+echo ISSigTool sign done
+
 goto exit
 
 :failed

build-ce.bat
@@ -87,7 +87,7 @@ if exist .\setup-presign.bat (
   echo Presign done
 )
 
-rem  Sign using user's private key
+rem  Sign using user's private key - also see compile.bat
 call .\issig.bat sign Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\SetupLdr.e32
 if errorlevel 1 goto failed
 echo ISSigTool sign done

build.bat
@@ -78,7 +78,7 @@ if exist .\setup-presign.bat (
   echo Presign done
 ) 
 
-rem  Sign using user's private key
+rem  Sign using user's private key - also see compile.bat
 call .\issig.bat sign Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\SetupLdr.e32
 if errorlevel 1 goto failed
 echo ISSigTool sign done

commit 192c532b4984a33e9570d0bf3eb5d2f2e4ad6ef4

Fix build.bat broken by 3c3ca70f.

compile.bat
@@ -90,6 +90,7 @@ if errorlevel 1 goto failed
 
 echo Success!
 
+if "%1"=="issigtool" goto exit
 rem  Sign using user's private key - will be overwritten if called by build.bat
 call .\issig.bat sign Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\SetupLdr.e32
 if errorlevel 1 goto failed