jrsoftware/issrc

commit 7b7dbd9eb881cd7ddd1fc00964399197c90b0065

More Integer64 cleanup.

Projects/Src/Setup.Install.HelperFunc.pas
@@ -15,7 +15,7 @@ unit Setup.Install.HelperFunc;
 interface
 
 uses
-  Windows, SHA256, Shared.FileClass, Shared.Int64Em, Shared.Struct, Setup.UninstallLog;
+  Windows, SHA256, Shared.FileClass, Shared.Struct, Setup.UninstallLog;
 
 type
   TSetupUninstallLog = class(TUninstallLog)
@@ -30,18 +30,17 @@ procedure SetStatusLabelText(const S: String;
   const ClearFilenameLabelText: Boolean = True);
 procedure InstallMessageBoxCallback(const Flags: LongInt; const After: Boolean;
   const Param: LongInt);
-procedure CalcFilesSize(var InstallFilesSize, AfterInstallFilesSize: Integer64);
-procedure InitProgressGauge(const InstallFilesSize: Integer64);
+procedure CalcFilesSize(var InstallFilesSize, AfterInstallFilesSize: Int64);
+procedure InitProgressGauge(const InstallFilesSize: Int64);
 procedure UpdateProgressGauge;
 procedure FinishProgressGauge(const HideGauge: Boolean);
-procedure SetProgress(const AProgress: Integer64);
-procedure IncProgress(const N: Cardinal);
-procedure IncProgress64(const N: Integer64);
-function CurProgress: Integer64;
+procedure SetProgress(const AProgress: Int64);
+procedure IncProgress(const N: Int64);
+function CurProgress: Int64;
 procedure ProcessEvents;
 procedure InternalProgressProc(const Bytes: Cardinal);
-procedure ExternalProgressProc64(const Bytes, MaxProgress: Integer64);
-procedure JustProcessEventsProc64(const Bytes, Param: Integer64);
+procedure ExternalProgressProc64(const Bytes, MaxProgress: Int64);
+procedure JustProcessEventsProc64(const Bytes, Param: Int64);
 function AbortRetryIgnoreTaskDialogMsgBox(const Text: String;
   const RetryIgnoreAbortButtonLabels: array of String): Boolean;
 function FileTimeToStr(const AFileTime: TFileTime): String;
@@ -49,7 +48,7 @@ function TryToGetSHA256OfFile(const DisableFsRedir: Boolean; const Filename: Str
   var Sum: TSHA256Digest): Boolean;
 procedure CopySourceFileToDestFile(const SourceF, DestF: TFile;
   [ref] const Verification: TSetupFileVerification; const ISSigSourceFilename: String;
-  const AExpectedSize: Integer64);
+  const AExpectedSize: Int64);
 function ShortenOrExpandFontFilename(const Filename: String): String;
 function GetLocalTimeAsStr: String;
 procedure PackCustomMessagesIntoString(var S: String);
@@ -119,70 +118,66 @@ begin
   SetAppTaskbarProgressState(States[NewState]);
 end;
 
-procedure CalcFilesSize(var InstallFilesSize, AfterInstallFilesSize: Integer64);
+procedure CalcFilesSize(var InstallFilesSize, AfterInstallFilesSize: Int64);
 var
   N: Integer;
   CurFile: PSetupFileEntry;
-  FileSize: Integer64;
 begin
-  InstallFilesSize := To64(0);
+  InstallFilesSize := 0;
   AfterInstallFilesSize := InstallFilesSize;
   for N := 0 to Entries[seFile].Count-1 do begin
     CurFile := PSetupFileEntry(Entries[seFile][N]);
     if ShouldProcessFileEntry(WizardComponents, WizardTasks, CurFile, False) then begin
       with CurFile^ do begin
+        var FileSize: Int64;
         if LocationEntry <> -1 then  { not an "external" file }
           FileSize := PSetupFileLocationEntry(Entries[seFileLocation][
            LocationEntry])^.OriginalSize
         else
           FileSize := ExternalSize;
-        Inc6464(InstallFilesSize, FileSize);
+        Inc(InstallFilesSize, FileSize);
         if not (foDeleteAfterInstall in Options) then
-          Inc6464(AfterInstallFilesSize, FileSize);
+          Inc(AfterInstallFilesSize, FileSize);
       end;
     end;
   end;
 end;
 
 var
-  CurProgressValue: Integer64;
+  CurProgressValue: Int64;
   ProgressShiftCount: Cardinal;
 
-procedure InitProgressGauge(const InstallFilesSize: Integer64);
-var
-  NewMaxValue: Integer64;
+procedure InitProgressGauge(const InstallFilesSize: Int64);
 begin
   { Calculate the MaxValue for the progress meter }
-  NewMaxValue := To64(1000 * Entries[seIcon].Count);
-  if Entries[seIni].Count <> 0 then Inc(NewMaxValue.Lo, 1000);
-  if Entries[seRegistry].Count <> 0 then Inc(NewMaxValue.Lo, 1000);
-  Inc6464(NewMaxValue, InstallFilesSize);
+  var NewMaxValue: Int64 := 1000 * Entries[seIcon].Count;
+  if Entries[seIni].Count <> 0 then Inc(NewMaxValue, 1000);
+  if Entries[seRegistry].Count <> 0 then Inc(NewMaxValue, 1000);
+  Inc(NewMaxValue, InstallFilesSize);
   { To avoid progress updates that are too small to result in any visible
     change, divide the Max value by 2 until it's under 1500 }
   ProgressShiftCount := 0;
-  while (NewMaxValue.Hi <> 0) or (NewMaxValue.Lo >= Cardinal(1500)) do begin
-    Shr64(NewMaxValue, 1);
+  while NewMaxValue >= 1500 do begin
+    NewMaxValue := NewMaxValue shr 1;
     Inc(ProgressShiftCount);
   end;
-  WizardForm.ProgressGauge.Max := NewMaxValue.Lo;
+  WizardForm.ProgressGauge.Max := NewMaxValue;
   SetMessageBoxCallbackFunc(InstallMessageBoxCallback, 0);
 end;
 
 procedure UpdateProgressGauge;
-var
-  NewPosition: Integer64;
 begin
-  NewPosition := CurProgressValue;
-  Shr64(NewPosition, ProgressShiftCount);
-  if WizardForm.ProgressGauge.Position <> Longint(NewPosition.Lo) then begin
-    WizardForm.ProgressGauge.Position := NewPosition.Lo;
+  var NewPosition := CurProgressValue;
+  NewPosition := NewPosition shr ProgressShiftCount;
+  if WizardForm.ProgressGauge.Position <> NewPosition then begin
+    WizardForm.ProgressGauge.Position := NewPosition;
     WizardForm.ProgressGauge.Update;
   end;
-  SetAppTaskbarProgressValue(NewPosition.Lo, WizardForm.ProgressGauge.Max);
+  SetAppTaskbarProgressValue(NewPosition, WizardForm.ProgressGauge.Max);
 
   if (CodeRunner <> nil) and CodeRunner.FunctionExists('CurInstallProgressChanged', True) then begin
     try
-      CodeRunner.RunProcedures('CurInstallProgressChanged', [NewPosition.Lo,
+      CodeRunner.RunProcedures('CurInstallProgressChanged', [NewPosition,
         WizardForm.ProgressGauge.Max], False);
     except
       Log('CurInstallProgressChanged raised an exception.');
@@ -201,25 +196,19 @@ begin
   SetAppTaskbarProgressState(tpsNoProgress);
 end;
 
-procedure SetProgress(const AProgress: Integer64);
+procedure SetProgress(const AProgress: Int64);
 begin
   CurProgressValue := AProgress;
   UpdateProgressGauge;
 end;
 
-procedure IncProgress(const N: Cardinal);
+procedure IncProgress(const N: Int64);
 begin
-  Inc64(CurProgressValue, N);
+  Inc(CurProgressValue, N);
   UpdateProgressGauge;
 end;
 
-procedure IncProgress64(const N: Integer64);
-begin
-  Inc6464(CurProgressValue, N);
-  UpdateProgressGauge;
-end;
-
-function CurProgress: Integer64;
+function CurProgress: Int64;
 begin
   Result := CurProgressValue;
 end;
@@ -241,20 +230,20 @@ begin
   ProcessEvents;
 end;
 
-procedure ExternalProgressProc64(const Bytes, MaxProgress: Integer64);
+procedure ExternalProgressProc64(const Bytes, MaxProgress: Int64);
 begin
   var NewProgress := CurProgress;
-  Inc6464(NewProgress, Bytes);
+  Inc(NewProgress, Bytes);
   { In case the source file was larger than we thought it was, stop the
     progress bar at the maximum amount. Also see CopySourceFileToDestFile. }
-  if Compare64(NewProgress, MaxProgress) > 0 then
+  if NewProgress > MaxProgress then
     NewProgress := MaxProgress;
   SetProgress(NewProgress);
   
   ProcessEvents;
 end;
 
-procedure JustProcessEventsProc64(const Bytes, Param: Integer64);
+procedure JustProcessEventsProc64(const Bytes, Param: Int64);
 begin
   ProcessEvents;
 end;
@@ -305,11 +294,10 @@ end;
 
 procedure CopySourceFileToDestFile(const SourceF, DestF: TFile;
   [ref] const Verification: TSetupFileVerification; const ISSigSourceFilename: String;
-  const AExpectedSize: Integer64);
+  const AExpectedSize: Int64);
 { Copies all bytes from SourceF to DestF, incrementing process meter as it
   goes. Assumes file pointers of both are 0. }
 var
-  BytesLeft: Integer64;
   BufSize: Cardinal;
   Buf: array[0..16383] of Byte;
   Context: TSHA256Context;
@@ -325,8 +313,8 @@ begin
   end;
 
   var MaxProgress := CurProgress;
-  Inc6464(MaxProgress, AExpectedSize);
-  BytesLeft := SourceF.Size;
+  Inc(MaxProgress, AExpectedSize);
+  var BytesLeft := SourceF.Size;
 
   { To avoid file system fragmentation, preallocate all of the bytes in the
     destination file }
@@ -336,19 +324,19 @@ begin
 
   while True do begin
     BufSize := SizeOf(Buf);
-    if (BytesLeft.Hi = 0) and (BytesLeft.Lo < BufSize) then
-      BufSize := BytesLeft.Lo;
+    if BytesLeft < BufSize then
+      BufSize := BytesLeft;
     if BufSize = 0 then
       Break;
 
     SourceF.ReadBuffer(Buf, BufSize);
     DestF.WriteBuffer(Buf, BufSize);
-    Dec64(BytesLeft, BufSize);
+    Dec(BytesLeft, BufSize);
 
     if Verification.Typ <> fvNone then
       SHA256Update(Context, Buf, BufSize);
 
-    ExternalProgressProc64(To64(BufSize), MaxProgress);
+    ExternalProgressProc64(BufSize, MaxProgress);
   end;
 
   if Verification.Typ <> fvNone then begin

Projects/Src/Compiler.SetupCompiler.pas
@@ -3108,7 +3108,7 @@ begin
       end;
     ssUninstallDisplaySize: begin
         if not StrToInteger64(Value, SetupHeader.UninstallDisplaySize) or
-           ((SetupHeader.UninstallDisplaySize.Lo = 0) and (SetupHeader.UninstallDisplaySize.Hi = 0)) then
+           (SetupHeader.UninstallDisplaySize = 0) then
           Invalid;
       end;
     ssUninstallFilesDir: begin
@@ -5270,7 +5270,7 @@ begin
         NoCompression := False;
         NoEncryption := False;
         SolidBreak := False;
-        ExternalSize := To64(0);
+        ExternalSize := 0;
         SortFilesByName := False;
         Sign := fsNoSetting;
 
@@ -7608,12 +7608,12 @@ var
             FL.FileVersionMS and $FFFF, FL.FileVersionLS shr 16,
             FL.FileVersionLS and $FFFF]);
         S := S + #9 + SHA256DigestToString(FL.SHA256Sum) + #9 +
-          Integer64ToStr(FL.OriginalSize) + #9 +
+          IntToStr(FL.OriginalSize) + #9 +
           SliceToString(FL.FirstSlice) + #9 +
           SliceToString(FL.LastSlice) + #9 +
           IntToStr(FL.StartOffset) + #9 +
-          Integer64ToStr(FL.ChunkSuboffset) + #9 +
-          Integer64ToStr(FL.ChunkCompressedSize) + #9 +
+          IntToStr(FL.ChunkSuboffset) + #9 +
+          IntToStr(FL.ChunkCompressedSize) + #9 +
           EncryptedStrings[floChunkEncrypted in FL.Flags] + #9 +
           FLExtraInfo.ISSigKeyUsedID;
         F.WriteLine(S);

Projects/Src/Compression.SevenZipDLLDecoder.pas
@@ -15,7 +15,7 @@ unit Compression.SevenZipDLLDecoder;
 interface
 
 uses
-  Windows, Shared.FileClass, Shared.VerInfoFunc, Shared.Int64Em, Compression.SevenZipDecoder;
+  Windows, Shared.FileClass, Shared.VerInfoFunc, Compression.SevenZipDecoder;
 
 function SevenZipDLLInit(const SevenZipLibrary: HMODULE;
   [ref] const VersionNumbers: TFileVersionNumbers): Boolean;
@@ -35,7 +35,7 @@ procedure ExtractArchiveRedir(const DisableFsRedir: Boolean;
   was found. }
 type
   TArchiveFindHandle = type NativeUInt;
-  TOnExtractToHandleProgress = procedure(const Bytes, Param: Integer64);
+  TOnExtractToHandleProgress = procedure(const Bytes, Param: Int64);
 function ArchiveFindFirstFileRedir(const DisableFsRedir: Boolean;
   const ArchiveFilename, DestDir, Password: String;
   const RecurseSubDirs, ExtractIntent: Boolean;
@@ -43,7 +43,7 @@ function ArchiveFindFirstFileRedir(const DisableFsRedir: Boolean;
 function ArchiveFindNextFile(const FindFile: TArchiveFindHandle; out FindFileData: TWin32FindData): Boolean;
 function ArchiveFindClose(const FindFile: TArchiveFindHandle): Boolean;
 procedure ArchiveFindExtract(const FindFile: TArchiveFindHandle; const DestF: TFile;
-  const OnExtractToHandleProgress: TOnExtractToHandleProgress; const OnExtractToHandleProgressParam: Integer64);
+  const OnExtractToHandleProgress: TOnExtractToHandleProgress; const OnExtractToHandleProgressParam: Int64);
 
 type
   TFileTimeHelper = record helper for TFileTime
@@ -195,7 +195,7 @@ type
     FIndex: UInt32;
     FDestF: TFile;
     FOnExtractToHandleProgress: TOnExtractToHandleProgress;
-    FOnExtractToHandleProgressParam: Integer64;
+    FOnExtractToHandleProgressParam: Int64;
     FPreviousProgress: UInt64;
   protected
     { IArchiveExtractCallback }
@@ -208,7 +208,7 @@ type
     constructor Create(const InArchive: IInArchive; const numItems: UInt32;
       const Password: String; const Index: UInt32; const DestF: TFile;
       const OnExtractToHandleProgress: TOnExtractToHandleProgress;
-      const OnExtractToHandleProgressParam: Integer64);
+      const OnExtractToHandleProgressParam: Int64);
     destructor Destroy; override;
   end;
 
@@ -854,7 +854,7 @@ end;
 constructor TArchiveExtractToHandleCallback.Create(const InArchive: IInArchive;
   const numItems: UInt32; const Password: String; const Index: UInt32;
   const DestF: TFile; const OnExtractToHandleProgress: TOnExtractToHandleProgress;
-  const OnExtractToHandleProgressParam: Integer64);
+  const OnExtractToHandleProgressParam: Int64);
 begin
   inherited Create(InArchive, numItems, Password);
   FIndex := Index;
@@ -919,7 +919,7 @@ begin
       System.TMonitor.Exit(FLock);
     end;
 
-    FOnExtractToHandleProgress(Integer64(Progress-FPreviousProgress), FOnExtractToHandleProgressParam);
+    FOnExtractToHandleProgress(Progress-FPreviousProgress, FOnExtractToHandleProgressParam);
     FPreviousProgress := Progress;
   end;
 end;
@@ -1246,7 +1246,7 @@ end;
 
 procedure ArchiveFindExtract(const FindFile: TArchiveFindHandle; const DestF: TFile;
   const OnExtractToHandleProgress: TOnExtractToHandleProgress;
-  const OnExtractToHandleProgressParam: Integer64);
+  const OnExtractToHandleProgressParam: Int64);
 begin
   const State = ArchiveFindStates[CheckFindFileHandle(FindFile)];
 

Projects/Src/Setup.DownloadFileFunc.pas
@@ -12,16 +12,16 @@ unit Setup.DownloadFileFunc;
 interface
 
 uses
-  Shared.Int64Em, Shared.FileClass, Shared.Struct;
+  Shared.FileClass, Shared.Struct;
 
 type
   TOnDownloadProgress = function(const Url, BaseName: string; const Progress, ProgressMax: Int64): Boolean of object;
-  TOnSimpleDownloadProgress = procedure(const Bytes, Param: Integer64);
+  TOnSimpleDownloadProgress = procedure(const Bytes, Param: Int64);
 
 function DownloadFile(const Url, CustomUserName, CustomPassword: String;
   const DestF: TFile; [ref] const Verification: TSetupFileVerification; const ISSigSourceFilename: String;
   const OnSimpleDownloadProgress: TOnSimpleDownloadProgress;
-  const OnSimpleDownloadProgressParam: Integer64): Int64;
+  const OnSimpleDownloadProgressParam: Int64): Int64;
 
   function DownloadTemporaryFile(const Url, BaseName: String;
   [ref] const Verification: TSetupFileVerification; const OnDownloadProgress: TOnDownloadProgress): Int64; overload;
@@ -60,7 +60,7 @@ type
       FDestFile: TFile;
       FOnDownloadProgress: TOnDownloadProgress;
       FOnSimpleDownloadProgress: TOnSimpleDownloadProgress;
-      FOnSimpleDownloadProgressParam: Integer64;
+      FOnSimpleDownloadProgressParam: Int64;
       FLock: TObject;
       FProgress, FProgressMax: Int64;
       FProgressSet: Boolean;
@@ -77,7 +77,7 @@ type
     property BaseName: String write FBaseName;
     property OnDownloadProgress: TOnDownloadProgress write FOnDownloadProgress;
     property OnSimpleDownloadProgress: TOnSimpleDownloadProgress write FOnSimpleDownloadProgress;
-    property OnSimpleDownloadProgressParam: Integer64 write FOnSimpleDownloadProgressParam;
+    property OnSimpleDownloadProgressParam: Int64 write FOnSimpleDownloadProgressParam;
     property Aborted: Boolean read FAbort;
     property Progress: Int64 read FProgress;
     property ProgressMax: Int64 read FProgressMax;
@@ -197,7 +197,7 @@ begin
           FAbort := True; { Atomic so no lock }
       end else if Assigned(FOnSimpleDownloadProgress) then begin
         try
-          FOnSimpleDownloadProgress(Integer64(Progress-FLastReportedProgress), FOnSimpleDownloadProgressParam);
+          FOnSimpleDownloadProgress(Progress-FLastReportedProgress, FOnSimpleDownloadProgressParam);
         finally
           FLastReportedProgress := Progress;
         end;
@@ -323,7 +323,7 @@ end;
 function DownloadFile(const Url, CustomUserName, CustomPassword: String;
   const DestF: TFile; [ref] const Verification: TSetupFileVerification; const ISSigSourceFilename: String;
   const OnSimpleDownloadProgress: TOnSimpleDownloadProgress;
-  const OnSimpleDownloadProgressParam: Integer64): Int64;
+  const OnSimpleDownloadProgressParam: Int64): Int64;
 var
   HTTPDataReceiver: THTTPDataReceiver;
 begin

Projects/Src/Setup.Install.pas
@@ -79,7 +79,7 @@ begin
 end;
 
 procedure RegisterUninstallInfo(const UninstLog: TUninstallLog; const UninstallRegKeyBaseName: String;
-  const AfterInstallFilesSize: Integer64);
+  const AfterInstallFilesSize: Int64);
 { Stores uninstall information in the Registry so that the program can be
   uninstalled through the Control Panel Add/Remove Programs applet. }
 const
@@ -340,7 +340,7 @@ begin
       SetDWordValue(H2, 'VersionMinor', MinorVersion);
     end;
     { Note: Windows 7 (and later?) doesn't automatically calculate sizes so set EstimatedSize ourselves. }
-    if (SetupHeader.UninstallDisplaySize.Hi = 0) and (SetupHeader.UninstallDisplaySize.Lo = 0) then begin
+    if SetupHeader.UninstallDisplaySize = 0 then begin
       { Estimate the size by taking the size of all files and adding any ExtraDiskSpaceRequired. }
       EstimatedSize := AfterInstallFilesSize;
       Inc6464(EstimatedSize, SetupHeader.ExtraDiskSpaceRequired);
@@ -513,7 +513,7 @@ type
 procedure ProcessFileEntry(const UninstLog: TUninstallLog; const ExpandedAppId: String;
   const RegisterFilesList: TList; const CurFile: PSetupFileEntry;
   const DisableFsRedir: Boolean; AExternalSourceFile, ADestFile: String;
-  const FileLocationFilenames: TStringList; const AExternalSize: Integer64;
+  const FileLocationFilenames: TStringList; const AExternalSize: Int64;
   var ConfirmOverwriteOverwriteAll, PromptIfOlderOverwriteAll: TOverwriteAll;
   var WarnedPerUserFonts: Boolean; const AExternalFileDate: PFileTime);
 { Not external: AExternalSourceFile and ADestFile should be empty strings,
@@ -688,7 +688,6 @@ procedure ProcessFileEntry(const UninstLog: TUninstallLog; const ExpandedAppId: 
 
 var
   ProgressUpdated: Boolean;
-  PreviousProgress: Integer64;
   LastOperation: String;
   CurFileLocation: PSetupFileLocationEntry;
   SourceFile, DestFile, TempFile, FontFilename: String;
@@ -725,7 +724,7 @@ Retry:
   TempFile := '';
   TempFileLeftOver := False;
   ProgressUpdated := False;
-  PreviousProgress := CurProgress;
+  var PreviousProgress := CurProgress;
   LastOperation := '';
   Failed := '';
   try
@@ -1078,7 +1077,7 @@ Retry:
               already been handled by RecurseExternalArchiveCopyFiles. }
             LastOperation := SetupMessages[msgErrorExtracting];
             var MaxProgress := CurProgress;
-            Inc6464(MaxProgress, AExternalSize);
+            Inc(MaxProgress, AExternalSize);
             ArchiveFindExtract(StrToInt(SourceFile), DestF, ExternalProgressProc64, MaxProgress);
           end
           else if foDownload in CurFile^.Options then begin
@@ -1088,7 +1087,7 @@ Retry:
             const DownloadUserName = ExpandConst(CurFile^.DownloadUserName);
             const DownloadPassword = ExpandConst(CurFile^.DownloadPassword);
             var MaxProgress := CurProgress;
-            Inc6464(MaxProgress, AExternalSize);
+            Inc(MaxProgress, AExternalSize);
             if CurFile^.Verification.Typ = fvISSig then begin
               const ISSigTempFile = TempFile + ISSigExt;
               const ISSigDestF = TFileRedir.Create(DisableFsRedir, ISSigTempFile, fdCreateAlways, faReadWrite, fsNone);
@@ -1096,7 +1095,7 @@ Retry:
                 { Download the .issig file }
                 const ISSigUrl = GetISSigUrl(SourceFile, ExpandConst(CurFile^.DownloadISSigSource));
                 DownloadFile(ISSigUrl, DownloadUserName, DownloadPassword,
-                  ISSigDestF, NoVerification, '', JustProcessEventsProc64, To64(0));
+                  ISSigDestF, NoVerification, '', JustProcessEventsProc64, 0);
                 FreeAndNil(ISSigDestF);
                 { Download and verify the actual file }
                 DownloadFile(SourceFile, DownloadUserName, DownloadPassword,
@@ -1393,9 +1392,9 @@ Retry:
   { Increment progress meter, if not already done so }
   if not ProgressUpdated then begin
     if Assigned(CurFileLocation) then  { not an "external" file }
-      IncProgress64(CurFileLocation^.OriginalSize)
+      IncProgress(CurFileLocation^.OriginalSize)
     else
-      IncProgress64(AExternalSize);
+      IncProgress(AExternalSize);
   end;
 
   { Process any events between copying files }
@@ -1411,7 +1410,7 @@ procedure CopyFiles(const UninstLog: TUninstallLog; const ExpandedAppId: String;
 
   function RecurseExternalCopyFiles(const DisableFsRedir: Boolean;
     const SearchBaseDir, SearchSubDir, SearchWildcard: String; const SourceIsWildcard: Boolean;
-    const Excludes: TStrings; const CurFile: PSetupFileEntry; var ExpectedBytesLeft: Integer64;
+    const Excludes: TStrings; const CurFile: PSetupFileEntry; var ExpectedBytesLeft: Int64;
     var ConfirmOverwriteOverwriteAll, PromptIfOlderOverwriteAll: TOverwriteAll;
     var WarnedPerUserFonts: Boolean): Boolean;
   begin
@@ -1460,7 +1459,7 @@ procedure CopyFiles(const UninstLog: TUninstallLog; const ExpandedAppId: String;
               CurFile, DisableFsRedir, SourceFile, DestFile, nil,
               Size, ConfirmOverwriteOverwriteAll, PromptIfOlderOverwriteAll,
               WarnedPerUserFonts, nil);
-            Dec6464(ExpectedBytesLeft, Size);
+            Dec(ExpectedBytesLeft, Size);
           end;
         until not FindNextFile(H, FindData);
       finally
@@ -1511,7 +1510,7 @@ procedure CopyFiles(const UninstLog: TUninstallLog; const ExpandedAppId: String;
 
   function RecurseExternalArchiveCopyFiles(const DisableFsRedir: Boolean;
     const ArchiveFilename: String; const Excludes: TStrings;
-    const CurFile: PSetupFileEntry; var ExpectedBytesLeft: Integer64;
+    const CurFile: PSetupFileEntry; var ExpectedBytesLeft: Int64;
     var ConfirmOverwriteOverwriteAll, PromptIfOlderOverwriteAll: TOverwriteAll;
     var WarnedPerUserFonts: Boolean): Boolean;
   begin
@@ -1591,7 +1590,7 @@ procedure CopyFiles(const UninstLog: TUninstallLog; const ExpandedAppId: String;
                 CurFile, DisableFsRedir, SourceFile, DestFile,
                 nil, Size, ConfirmOverwriteOverwriteAll, PromptIfOlderOverwriteAll,
                 WarnedPerUserFonts, @FindData.ftLastWriteTime);
-              Dec6464(ExpectedBytesLeft, Size);
+              Dec(ExpectedBytesLeft, Size);
             end else if foCreateAllSubDirs in CurFile.Options then begin
               var Flags: TMakeDirFlags := [];
               if foUninsNeverUninstall in CurFile^.Options then Include(Flags, mdNoUninstall);
@@ -1617,7 +1616,6 @@ var
   CurFileNumber: Integer;
   CurFile: PSetupFileEntry;
   SourceWildcard: String;
-  ProgressBefore, ExpectedBytesLeft: Integer64;
   DisableFsRedir, FoundFiles: Boolean;
   ConfirmOverwriteOverwriteAll, PromptIfOlderOverwriteAll: TOverwriteAll;
   WarnedPerUserFonts: Boolean;
@@ -1655,7 +1653,7 @@ begin
 
         if CurFile^.LocationEntry <> -1 then begin
           ProcessFileEntry(UninstLog, ExpandedAppId, RegisterFilesList,
-            CurFile, DisableFsRedir, '', '', FileLocationFilenames, To64(0),
+            CurFile, DisableFsRedir, '', '', FileLocationFilenames, 0,
             ConfirmOverwriteOverwriteAll, PromptIfOlderOverwriteAll, WarnedPerUserFonts, nil);
         end
         else begin
@@ -1668,10 +1666,10 @@ begin
           else
             SourceWildcard := ExpandConst(CurFile^.SourceFilename);
           Excludes.DelimitedText := CurFile^.Excludes;
-          ProgressBefore := CurProgress;
+          var ProgressBefore := CurProgress;
           repeat
             SetProgress(ProgressBefore);
-            ExpectedBytesLeft := CurFile^.ExternalSize;
+            var ExpectedBytesLeft := CurFile^.ExternalSize;
             if foDownload in CurFile^.Options then begin
               { Archive download should have been done already by Setup.WizardForm's DownloadArchivesToExtract }
               if foExtractArchive in CurFile^.Options then
@@ -1704,7 +1702,7 @@ begin
                   [SetupMessages[msgAbortRetryIgnoreRetry], SetupMessages[msgFileAbortRetryIgnoreSkipNotRecommended], SetupMessages[msgAbortRetryIgnoreCancel]]);
           { In case we didn't end up copying all the expected bytes, bump
             the progress bar up to the expected amount }
-          Inc6464(ProgressBefore, CurFile^.ExternalSize);
+          Inc(ProgressBefore, CurFile^.ExternalSize);
           SetProgress(ProgressBefore);
         end;
 
@@ -2765,11 +2763,11 @@ var
   Uninstallable, UninstLogCleared: Boolean;
   I: Integer;
   UninstallRegKeyBaseName: String;
-  InstallFilesSize, AfterInstallFilesSize: Integer64;
 begin
   Succeeded := False;
   Log('Starting the installation process.');
   SetCurrentDir(WinSystemDir);
+  var InstallFilesSize, AfterInstallFilesSize: Int64;
   CalcFilesSize(InstallFilesSize, AfterInstallFilesSize);
   InitProgressGauge(InstallFilesSize);
   UninstallExeCreated := ueNone;

commit 63cfb26cde09c2bf16f6755b8b294d298ab98d9b

Cherry pick into is-6_5_4: Fix bug introduced by 7b7dbd9e: caused CodeRunner.RunProcedures to (unnecessarily) receive a Int64 NewPosition and it refuses that.

Projects/Src/Setup.Install.HelperFunc.pas
@@ -167,8 +167,7 @@ end;
 
 procedure UpdateProgressGauge;
 begin
-  var NewPosition := CurProgressValue;
-  NewPosition := NewPosition shr ProgressShiftCount;
+  var NewPosition := Integer(CurProgressValue shr ProgressShiftCount);
   if WizardForm.ProgressGauge.Position <> NewPosition then begin
     WizardForm.ProgressGauge.Position := NewPosition;
     WizardForm.ProgressGauge.Update;