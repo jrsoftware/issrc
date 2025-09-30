50f48f9af8ca124c0c14567f6ee204f48a36d21f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-09-30 21:53:17 +0200

    Simplify MD5 and SHA1 by making them System.Hash wrappers like SHA256. Fixes a bunch of warnings. Todo: test.

--- Components/SHA256.pas
+++ Components/SHA256.pas
@@ -1,7 +1,7 @@
 unit SHA256;
 
 {
-  SHA256.pas: System.Hash.pas wrapper in the style of MD5.pas and SHA1.pas 
+  SHA256.pas: System.Hash.pas wrapper in the style of the old versions of MD5.pas and SHA1.pas
   Author: Martijn Laan
   License for SHA256.pas: Public domain, no copyright claimed
 }
