81a5912e84b5e3b6f99c5a37b8564b3d1a9a511a
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-09-30 21:50:35 +0200

    Cleanup.

--- Components/SHA256.pas
+++ Components/SHA256.pas
@@ -9,7 +9,7 @@ unit SHA256;
 interface
 
 uses
-  SysUtils, System.Hash;
+  System.Hash;
 
 type
   TSHA256Context = record
@@ -27,6 +27,9 @@ function SHA256DigestToString(const D: TSHA256Digest): String;
 function SHA256DigestFromString(const S: String): TSHA256Digest;
 
 implementation
+
+uses
+  System.SysUtils;
 
 procedure SHA256Init(var ctx: TSHA256Context);
 begin
