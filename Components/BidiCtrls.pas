871d95996aff791d3836b5d99dc217a29b6a50a0
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-10-06 21:14:56 +0200

    Properly center glyphs vertically on the first text line. Also add a few comments.

--- Components/BidiCtrls.pas
+++ Components/BidiCtrls.pas
@@ -150,6 +150,7 @@ end;
   -Improves alignment of shield icons, especially at high dpi
   -Avoids drawing empty notes
   -Respects the font of the control
+  -Properly centers glyphs vertically on the first text line
   For other button styles it just calls the original code, and the code for those styles is not copied here }
 
 class function TNewButtonStyleHook.DrawOrMeasureCommandLink(const Draw: Boolean;
@@ -157,7 +158,7 @@ class function TNewButtonStyleHook.DrawOrMeasureCommandLink(const Draw: Boolean;
   const ACanvas: TCanvas; const AMouseInControl: Boolean): Integer;
 var
   Details:  TThemedElementDetails;
-  LParentRect, DrawRect, R: TRect;
+  LParentRect, DrawRect, R, RSingleLine: TRect;
   LIsRightToLeft: Boolean;
   IL: BUTTON_IMAGELIST;
   IW, IH: Integer;
@@ -210,25 +211,6 @@ begin
   if Draw then
     LStyle.DrawElement(ACanvas.Handle, Details, DrawRect);
 
-  if Button_GetImageList(handle, IL) and (IL.himl <> 0) and
-     ImageList_GetIconSize(IL.himl, IW, IH) then
-  begin
-    R := DrawRect;
-    Inc(R.Left, 2);
-    Inc(R.Top, 15);
-    if IsElevationRequired then
-    begin
-      ImgIndex := 0;
-      Inc(R.Left, MulDiv(8, LPPI, Screen.DefaultPixelsPerInch));
-    end;
-    R.Right := R.Left + IW;
-    R.Bottom := R.Top + IH;
-    if Draw then begin
-      FlipRect(R, LParentRect, LIsRightToLeft);
-      ImageList_Draw(IL.himl, ImgIndex, ACanvas.Handle, R.Left, R.Top, ILD_NORMAL);
-    end;
-  end;
-
   IW := MulDiv(35, LPPI, Screen.DefaultPixelsPerInch);
   Inc(DrawRect.Left, IW);
   Inc(DrawRect.Top, 15);
@@ -236,9 +218,13 @@ begin
   ACanvas.Font := TNewButton(Control).Font;
   R := DrawRect;
   TextFormat := TTextFormatFlags(Control.DrawTextBiDiModeFlags(DT_LEFT or DT_WORDBREAK or DT_CALCRECT));
-  LStyle.DrawText(ACanvas.Handle, Details, BCaption, R, TextFormat, ACanvas.Font.Color);
+  LStyle.DrawText(ACanvas.Handle, Details, BCaption, R, TextFormat, ACanvas.Font.Color); { R is used below for the note }
+  RSingleLine := DrawRect;
+  TextFormat := TTextFormatFlags(Control.DrawTextBiDiModeFlags(DT_LEFT or DT_SINGLELINE or DT_CALCRECT));
+  LStyle.DrawText(ACanvas.Handle, Details, BCaption, RSingleLine, TextFormat, ACanvas.Font.Color); { RSingleLine is used below for the glyphs }
   Result := R.Bottom;
   if Draw then begin
+    { Does not use any DT_CALCRECT results }
     TextFormat := TTextFormatFlags(Control.DrawTextBiDiModeFlags(DT_LEFT or DT_WORDBREAK));
     if (seFont in Control.StyleElements) and LStyle.GetElementColor(Details, ecTextColor, ThemeTextColor) then
        ACanvas.Font.Color := ThemeTextColor;
@@ -256,18 +242,37 @@ begin
       ACanvas.Font.Height := MulDiv(ACanvas.Font.Height, 2, 3);
       R := DrawRect;
       TextFormat := TTextFormatFlags(Control.DrawTextBiDiModeFlags(DT_LEFT or DT_WORDBREAK or DT_CALCRECT));
-      LStyle.DrawText(ACanvas.Handle, Details, Buffer, R, TextFormat, ACanvas.Font.Color);
+      LStyle.DrawText(ACanvas.Handle, Details, Buffer, R, TextFormat, ACanvas.Font.Color);  { R is used directly below for measuring }
       if R.Bottom > Result then
         Result := R.Bottom;
       if Draw then begin
+        { Does not use any DT_CALCRECT results }
         TextFormat := TTextFormatFlags(Control.DrawTextBiDiModeFlags(DT_LEFT or DT_WORDBREAK));
         FlipRect(DrawRect, LParentRect, LIsRightToLeft);
         LStyle.DrawText(ACanvas.Handle, Details, Buffer, DrawRect, TextFormat, ACanvas.Font.Color);
       end;
     end;
   end;
-  if IL.himl = 0 then
+
+  Inc(Result, 15);
+
+  if Button_GetImageList(handle, IL) and (IL.himl <> 0) and
+     ImageList_GetIconSize(IL.himl, IW, IH) then
   begin
+    R.Left := 2;
+    R.Top := RSingleLine.Top + (RSingleLine.Height - IH) div 2;
+    if IsElevationRequired then
+    begin
+      ImgIndex := 0;
+      Inc(R.Left, MulDiv(8, LPPI, Screen.DefaultPixelsPerInch));
+    end;
+    R.Right := R.Left + IW;
+    R.Bottom := R.Top + IH;
+    if Draw then begin
+      FlipRect(R, LParentRect, LIsRightToLeft);
+      ImageList_Draw(IL.himl, ImgIndex, ACanvas.Handle, R.Left, R.Top, ILD_NORMAL);
+    end;
+  end else begin
     if not Control.Enabled then
       Details := LStyle.GetElementDetails(tbCommandLinkGlyphDisabled)
     else
@@ -282,7 +287,7 @@ begin
 
     DrawRect.Right := IW;
     DrawRect.Left := 3;
-    DrawRect.Top := 10;
+    DrawRect.Top := RSingleLine.Top + (RSingleLine.Height - IW) div 2;
     DrawRect.Bottom := DrawRect.Top + IW;
     if Draw then begin
       if LIsRightToLeft then begin
@@ -302,8 +307,6 @@ begin
         LStyle.DrawElement(ACanvas.Handle, Details, DrawRect, nil, LPPI);
     end;
   end;
-
-  Inc(Result, 15);
 end;
 
 class function TNewButtonStyleHook.GetIdealHeightIfCommandLink(const Control: TNewButton; const Style: TCustomStyleServices): Integer;
