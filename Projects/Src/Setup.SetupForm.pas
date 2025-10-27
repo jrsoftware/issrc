74eaa4590963166f6242ba6eec64e10a99d7823c
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-10-27 16:09:22 +0100

    Fix 37b18def which broke the fix from bf4a2b90.

--- Projects/Src/Setup.SetupForm.pas
+++ Projects/Src/Setup.SetupForm.pas
@@ -588,16 +588,23 @@ begin
   FKeepSizeY := KeepSizeY;
   FOrgClientWidth := ClientWidth;
   FOrgClientHeight := ClientHeight;
-  if ShouldSizeX then
-    ClientWidth := MulDiv(ClientWidth, SetupHeader.WizardSizePercentX, 100);
-  if ShouldSizeY then
-    ClientHeight := MulDiv(ClientHeight, SetupHeader.WizardSizePercentY, 100);
-  if HasCustomAnchors and ((ClientWidth <> FOrgClientWidth) or (FOrgClientHeight <> ClientHeight)) then begin
+
+  const LShouldSizeX = ShouldSizeX;
+  const LShouldSizeY = ShouldSizeY;
+
+  if HasCustomAnchors and (LShouldSizeX or LShouldSizeY) then begin
     { Various things related to positioning and anchoring don't work without this:
       you get positions of child controls back as if there was no anchoring until
-      handles are automatically created }
+      handles are automatically created. For WizardForm it works if done after
+      sizing but for UninstallProgressForm it must be done before sizing (for
+      unknown reasons), so doing before sizing. }
     ParentHandlesNeeded(Self); { Also see ShowModal }
   end;
+
+  if LShouldSizeX then
+    ClientWidth := MulDiv(ClientWidth, SetupHeader.WizardSizePercentX, 100);
+  if LShouldSizeY then
+    ClientHeight := MulDiv(ClientHeight, SetupHeader.WizardSizePercentY, 100);
 end;
 
 function TSetupForm.GetExtraClientWidth: Integer;
