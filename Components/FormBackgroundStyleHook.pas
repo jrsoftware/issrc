5227c55afac6296402be1beaa24cb3068f879407
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-11-04 21:05:31 +0100

    Make the hook use TBitmapImageImplementation so it can paint images and/or a back color.

--- Components/FormBackgroundStyleHook.pas
+++ Components/FormBackgroundStyleHook.pas
@@ -12,11 +12,22 @@ unit FormBackgroundStyleHook;
 interface
 
 uses
-  Vcl.Forms, Vcl.Graphics;
+  Vcl.Forms, Vcl.Controls, Vcl.Graphics,
+  BitmapImage;
 
 type
   TFormBackgroundStyleHook = class(TFormStyleHook)
+  private
+    class destructor Destroy;
+    class var FBitmapImageImpl: TBitmapImageImplementation;
+    class var FBackColor: TColor;
+    class var FGraphic: TGraphic;
+    class var FCurrentControl: TControl;
+  protected
     procedure PaintBackground(Canvas: TCanvas); override;
+  public
+    class property BackColor: TColor write FBackColor;
+    class property Graphic: TGraphic write FGraphic;
   end;
 
 implementation
@@ -26,10 +37,26 @@ uses
 
 { TFormBackgroundStyleHook }
 
+class destructor TFormBackgroundStyleHook.Destroy;
+begin
+  if FCurrentControl <> nil then
+    FBitmapImageImpl.DeInit;
+end;
+
 procedure TFormBackgroundStyleHook.PaintBackground(Canvas: TCanvas);
 begin
-  Canvas.Brush.Color := clRed;
-  Canvas.FillRect(Rect(0, 0, Control.Width, Control.Height));
+  if Control <> FCurrentControl then begin
+    if FCurrentControl <> nil then
+      FBitmapImageImpl.DeInit;
+    FCurrentControl := Control;
+    FBitmapImageImpl.Init(FCurrentControl);
+    FBitmapImageImpl.BackColor := FBackColor;
+    if FGraphic <> nil then
+      FBitmapImageImpl.SetGraphic(FGraphic);
+  end;
+
+  var R := Rect(0, 0, Control.Width, Control.Height);
+  FBitmapImageImpl.Paint(Self, Canvas, R);
 end;
 
 end.
