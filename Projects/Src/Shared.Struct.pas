5282b77d3a06e09aad0913445704c2fd955014fb
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-11-12 09:26:14 +0100

    Update version.

--- Projects/Src/Shared.Struct.pas
+++ Projects/Src/Shared.Struct.pas
@@ -17,8 +17,8 @@ uses
 
 const
   SetupTitle = 'Inno Setup';
-  SetupVersion = '6.6.0';
-  SetupBinVersion = (6 shl 24) + (6 shl 16) + (0 shl 8) + 0;
+  SetupVersion = '6.6.1-dev';
+  SetupBinVersion = (6 shl 24) + (6 shl 16) + (1 shl 8) + 0;
 
 type
   TSetupID = array[0..63] of AnsiChar;
