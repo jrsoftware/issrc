38be350ec0b1dfc21bcbef98886683d58f8fb0d6
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2025-12-27 11:52:38 +0100

    Update C and Delphi MyDll examples.

--- Examples/MyDll/C/MyDll.c
+++ Examples/MyDll/C/MyDll.c
@@ -1,6 +1,6 @@
 #include <windows.h>
 
-void __stdcall MyDllFunc(HWND hWnd, char *lpText, char *lpCaption, UINT uType)
+void __stdcall MyDllFunc(HWND hWnd, wchar_t *lpText, wchar_t *lpCaption, UINT uType)
 {
   MessageBox(hWnd, lpText, lpCaption, uType);
 }
