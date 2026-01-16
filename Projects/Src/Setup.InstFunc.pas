37a1cee23e6c4342c767bf20683117dddb2a1236
Jordan Russell <jr-github@jrsoftware.org>
2026-01-16 03:36:31 -0600

    Fix 85a7c1e: The destination filenames (usually) have both prefixes.

--- Projects/Src/Setup.InstFunc.pas
+++ Projects/Src/Setup.InstFunc.pas
@@ -938,15 +938,15 @@ procedure EnumFileReplaceOperationsFilenames(const EnumFunc: TEnumFROFilenamesPr
     P := PChar(S);
     PEnd := P + Length(S);
     while P < PEnd do begin
-      if P[0] = '!' then
-        { Note: '!' means that MoveFileEx was called with the
-          MOVEFILE_REPLACE_EXISTING flag }
-        Inc(P)
-      else if (P[0] = '*') and CharInSet(P[1], ['1', '2']) then
+      if (P[0] = '*') and CharInSet(P[1], ['1', '2']) then
         { Note: '*1' and '*2' _seem_ to mean
           - On the source filename, *1 means the file exists, *2 otherwise
           - On the destination filename, *1 means the path exists, *2 otherwise }
         Inc(P, 2);
+      if P[0] = '!' then
+        { Note: '!' means that MoveFileEx was called with the
+          MOVEFILE_REPLACE_EXISTING flag }
+        Inc(P);
       if StrLComp(P, '\??\', 4) = 0 then begin
         Inc(P, 4);
         if P[0] <> #0 then
