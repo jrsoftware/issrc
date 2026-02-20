b191f94d1535a58250375a94acc297adb4c5bc3f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-02-20 09:53:06 +0100

    Bump SetupBinVersion build number.

--- Projects/Src/Shared.Struct.pas
+++ Projects/Src/Shared.Struct.pas
@@ -18,7 +18,7 @@ uses
 const
   SetupTitle = 'Inno Setup';
   SetupVersion = '7.0.0-dev';
-  SetupBinVersion = (7 shl 24) + (0 shl 16) + (0 shl 8) + 0;
+  SetupBinVersion = (7 shl 24) + (0 shl 16) + (0 shl 8) + 1;
 
 type
   TSetupID = array[0..63] of AnsiChar;
