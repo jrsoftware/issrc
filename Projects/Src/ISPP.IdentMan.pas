fb65dd1c03a3e31144b322067b257ca78e234cfd
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-03-29 14:37:08 +0200

    Update headers.

--- Projects/Src/ISPP.IdentMan.pas
+++ Projects/Src/ISPP.IdentMan.pas
@@ -3,7 +3,7 @@
   Copyright (C) 2001-2002 Alex Yackimoff
 
   Inno Setup
-  Copyright (C) 1997-2025 Jordan Russell
+  Copyright (C) 1997-2026 Jordan Russell
   Portions by Martijn Laan
   For conditions of distribution and use, see LICENSE.TXT.
 }
