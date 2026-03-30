ea7d9e84503ff4224a24eacbe039190d52753821
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-03-30 09:26:52 +0200

    Fix comment typos.

--- Projects/Src/IDE.IDEScintEdit.pas
+++ Projects/Src/IDE.IDEScintEdit.pas
@@ -382,9 +382,9 @@ begin
   FComplexCommandsReversed.Clear;
 
   { Normally VK_OEM_1 is ;, VK_OEM_6 is ], VK_OEM_4 is [,  VK_OEM_2 is /,  and VK_OEM_5 is \
-    See IDE.HelperFunc's NewShortcutToText for how it's is handled when they are different.
+    See IDE.HelperFunc's NewShortcutToText for how it is handled when they are different.
     Note: all VK_OEM shortcuts must have a menu item so the user can see what the
-    shortcut is for their kayboard layout. }
+    shortcut is for their keyboard layout. }
 
   if FKeyMappingType = kmtVSCode then begin
     { Use freed Ctrl+D and Ctrl+Shift+L }
