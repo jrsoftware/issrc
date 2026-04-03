jrsoftware/issrc

commit d1370802055f9c22620c85819dc0c385194dc52c

Remove duplicate code.

Components/BitmapButton.pas
@@ -18,25 +18,18 @@ unit BitmapButton;
 interface
 
 uses
-  Windows, Messages, Controls, Graphics, Classes;
+  Windows, Messages, Controls, Graphics, Classes,
+  BitmapImage;
 
 type
   TPaintEvent = procedure(Sender: TObject; Canvas: TCanvas; var ARect: TRect) of object;
 
   TBitmapButton = class(TCustomControl)
   private
-    FAutoSize: Boolean;
-    FBackColor: TColor;
-    FBitmap: TBitmap;
-    FCenter: Boolean;
+    FImpl: TBitmapImageImplementation;
     FOnClick: TNotifyEvent;
     FOnDblClick: TNotifyEvent;
     FOnPaint: TPaintEvent;
-    FReplaceColor: TColor;
-    FReplaceWithColor: TColor;
-    FStretch: Boolean;
-    FStretchedBitmap: TBitmap;
-    FStretchedBitmapValid: Boolean;
     procedure BitmapChanged(Sender: TObject);
     procedure SetBackColor(Value: TColor);
     procedure SetBitmap(Value: TBitmap);
@@ -59,18 +52,18 @@ type
   published
     property Align;
     property Anchors;
-    property AutoSize: Boolean read FAutoSize write SetAutoSize default False;
-    property BackColor: TColor read FBackColor write SetBackColor default clNone;
+    property AutoSize: Boolean read FImpl.AutoSize write SetAutoSize default False;
+    property BackColor: TColor read FImpl.BackColor write SetBackColor default clNone;
     property Caption;
-    property Center: Boolean read FCenter write SetCenter default False;
+    property Center: Boolean read FImpl.Center write SetCenter default False;
     property Enabled;
     property ParentShowHint;
-    property Bitmap: TBitmap read FBitmap write SetBitmap;
+    property Bitmap: TBitmap read FImpl.Bitmap write SetBitmap;
     property PopupMenu;
     property ShowHint;
-    property Stretch: Boolean read FStretch write SetStretch default False;
-    property ReplaceColor: TColor read FReplaceColor write SetReplaceColor default clNone;
-    property ReplaceWithColor: TColor read FReplaceWithColor write SetReplaceWithColor default clNone;
+    property Stretch: Boolean read FImpl.Stretch write SetStretch default False;
+    property ReplaceColor: TColor read FImpl.ReplaceColor write SetReplaceColor default clNone;
+    property ReplaceWithColor: TColor read FImpl.ReplaceWithColor write SetReplaceWithColor default clNone;
     property TabOrder;
     property TabStop default True;
     property Visible;
@@ -93,57 +86,20 @@ end;
 
 function TBitmapButton.InitializeFromIcon(const Instance: HINST; const Name: PChar; const BkColor: TColor; const AscendingTrySizes: array of Integer): Boolean;
 begin
-  { Find the largest regular icon size smaller than the scaled image }
-  var Size := 0;
-  for var I := Length(AscendingTrySizes)-1 downto 0 do begin
-    if (Width >= AscendingTrySizes[I]) and (Height >= AscendingTrySizes[I]) then begin
-      Size := AscendingTrySizes[I];
-      Break;
-    end;
-  end;
-  if Size = 0 then
-    Size := Min(Width, Height);
+  Result := FImpl.InitializeFromIcon(HInstance, Name, BkColor, AscendingTrySizes);
+end;
 
-  { Load the desired icon }
-  var Flags := LR_DEFAULTCOLOR;
-  if Instance = 0 then
-    Flags := Flags or LR_LOADFROMFILE;
-  var Handle := LoadImage(Instance, Name, IMAGE_ICON, Size, Size, Flags);
-  if Handle = 0 then
-    Handle := LoadImage(Instance, Name, IMAGE_ICON, 0, 0, Flags);
-  if Handle <> 0 then begin
-    const Icon = TIcon.Create;
-    try
-      Icon.Handle := Handle;
-
-      { Set sizes (overrides any scaling) }
-      Width := Icon.Width;
-      Height := Icon.Height;
-
-      { Draw icon into bitmap }
-      Bitmap.Canvas.Brush.Color := BkColor;
-      Bitmap.Width := Width;
-      Bitmap.Height := Height;
-      Bitmap.Canvas.Draw(0, 0, Icon);
-
-      Result := True;
-    finally
-      Icon.Free;
-    end;
-  end else
-    Result := False;
+procedure TBitmapButton.BitmapChanged(Sender: TObject);
+begin
+  FImpl.BitmapChanged(Sender)
 end;
 
 constructor TBitmapButton.Create(AOwner: TComponent);
 begin
   inherited;
   ControlStyle := ControlStyle + [csReplicatable];
-  FBackColor := clNone;
-  FBitmap := TBitmap.Create;
-  FBitmap.OnChange := BitmapChanged;
-  FReplaceColor := clNone;
-  FReplaceWithColor := clNone;
-  FStretchedBitmap := TBitmap.Create;
+  FImpl := Default(TBitmapImageImplementation);
+  FImpl.Init(Self, BitmapChanged);
   TabStop := True;
   Height := 105;
   Width := 105;
@@ -151,91 +107,61 @@ end;
 
 procedure TBitmapButton.CreateParams(var Params: TCreateParams);
 begin
-  inherited CreateParams(Params);
+  inherited;
   CreateSubClass(Params, 'BUTTON');
 end;
 
 destructor TBitmapButton.Destroy;
 begin
-  FStretchedBitmap.Free;
-  FBitmap.Free;
-  inherited Destroy;
-end;
-
-procedure TBitmapButton.BitmapChanged(Sender: TObject);
-begin
-  FStretchedBitmapValid := False;
-  if FAutoSize and (FBitmap.Width > 0) and (FBitmap.Height > 0) then
-    SetBounds(Left, Top, FBitmap.Width, FBitmap.Height);
-  if (FBitmap.Width >= Width) and (FBitmap.Height >= Height) then
-    ControlStyle := ControlStyle + [csOpaque] - [csParentBackground]
-  else
-    ControlStyle := ControlStyle - [csOpaque] + [csParentBackground];
-  Invalidate;
+  FImpl.DeInit;
+  inherited;
 end;
 
 procedure TBitmapButton.SetAutoSize(Value: Boolean);
 begin
-  FAutoSize := Value;
-  BitmapChanged(Self);
+  FImpl.SetAutoSize(Self, Value);
 end;
 
 procedure TBitmapButton.SetBackColor(Value: TColor);
 begin
-  if FBackColor <> Value then begin
-    FBackColor := Value;
-    BitmapChanged(Self);
-  end;
+  FImpl.SetBackColor(Self, Value);
 end;
 
 procedure TBitmapButton.SetBitmap(Value: TBitmap);
 begin
-  FBitmap.Assign(Value);
+  FImpl.SetBitmap(Value);
 end;
 
 procedure TBitmapButton.SetCenter(Value: Boolean);
 begin
-  if FCenter <> Value then begin
-    FCenter := Value;
-    BitmapChanged(Self);
-  end;
+  FImpl.SetCenter(Self, Value);
 end;
 
 procedure TBitmapButton.SetReplaceColor(Value: TColor);
 begin
-  if FReplaceColor <> Value then begin
-    FReplaceColor := Value;
-    BitmapChanged(Self);
-  end;
+  FImpl.SetReplaceColor(Self, Value);
 end;
 
 procedure TBitmapButton.SetReplaceWithColor(Value: TColor);
 begin
-  if FReplaceWithColor <> Value then begin
-    FReplaceWithColor := Value;
-    BitmapChanged(Self);
-  end;
+  FImpl.SetReplaceWithColor(Self, Value);
 end;
 
 procedure TBitmapButton.SetStretch(Value: Boolean);
 begin
-  if FStretch <> Value then begin
-    FStretch := Value;
-    FStretchedBitmap.Assign(nil);
-    BitmapChanged(Self);
-  end;
+  FImpl.SetStretch(Self, Value);
 end;
 
 function TBitmapButton.GetPalette: HPALETTE;
 begin
-  Result := FBitmap.Palette;
+  Result := FImpl.GetPalette;
 end;
 
 procedure TBitmapButton.Paint;
 begin
   Canvas.Font := Font;
   Canvas.Brush.Color := Color;
-  
+
   var R := ClientRect;
 
   if Focused then begin
@@ -254,69 +180,7 @@ begin
 
   InflateRect(R, -FocusBorderWidth, -FocusBorderHeight);
 
-  const Is32bit = (FBitmap.PixelFormat = pf32bit) and
-    (FBitmap.AlphaFormat in [afDefined, afPremultiplied]);
-
-  var W, H: Integer;
-  var Bmp: TBitmap;
-  if Stretch then begin
-    W := R.Width;
-    H := R.Height;
-    Bmp := FStretchedBitmap;
-    if not FStretchedBitmapValid or (FStretchedBitmap.Width <> W) or
-       (FStretchedBitmap.Height <> H) then begin
-      FStretchedBitmapValid := True;
-      if (FBitmap.Width = W) and (FBitmap.Height = H) then
-        FStretchedBitmap.Assign(FBitmap)
-      else begin
-        FStretchedBitmap.Assign(nil);
-        if not StretchBmp(FBitmap, FStretchedBitmap, W, H, Is32bit) then begin
-          if Is32bit then begin
-            FStretchedBitmapValid := False;
-            Bmp := FBitmap;
-          end else begin
-            FStretchedBitmap.Palette := CopyPalette(FBitmap.Palette);
-            FStretchedBitmap.Width := W;
-            FStretchedBitmap.Height := H;
-            FStretchedBitmap.Canvas.StretchDraw(R, FBitmap);
-          end;
-        end;
-      end;
-    end;
-  end else begin
-    Bmp := FBitmap;
-    W := Bmp.Width;
-    H := Bmp.Height;
-  end;
-
-  if (FBackColor <> clNone) and (Is32Bit or (Bmp.Width < Width) or (Bmp.Height < Height)) then begin
-    Canvas.Brush.Style := bsSolid;
-    Canvas.Brush.Color := FBackColor;
-    Canvas.FillRect(R);
-  end;
-
-  if csDesigning in ComponentState then begin
-    Canvas.Pen.Style := psDash;
-    Canvas.Brush.Style := bsClear;
-    Canvas.Rectangle(0, 0, Width, Height);
-  end;
-
-  var X := R.Left;
-  var Y := R.Top;
-  if Center then begin
-    Inc(X, (R.Width - W) div 2);
-    if X < 0 then
-      X := 0;
-    Inc(Y, (R.Height - H) div 2);
-    if Y < 0 then
-      Y := 0;
-  end;
-
-  if not Is32bit and (FReplaceColor <> clNone) and (FReplaceWithColor <> clNone) then begin
-    Canvas.Brush.Color := FReplaceWithColor;
-    Canvas.BrushCopy(Rect(X, Y, X + W, Y + H), Bmp, Rect(0, 0, Bmp.Width, Bmp.Height), FReplaceColor);
-  end else
-    Canvas.Draw(X, Y, Bmp);
+  FImpl.Paint(Canvas, R);
 
   if Assigned(FOnPaint) then
     FOnPaint(Self, Canvas, R);

commit 915ed64945c6b348d929a7f1a0bfa942d184f514

Cherry pick into is6: -[Files] section entries with the extractarchive flag could fail to extract any files from an archive. -ISPP: Fix handling of relative filenames by support functions ReadIni, WriteIni, and Is64BitPEImage. -Fix support functions InitializeBitmapButtonFromIcon and InitializeBitmapImageFromIcon, broken by d1370802. -Fix CreateIniEntries bug. Choosing Ignore on MakeDir error would not work correctly. -Fix constant (like '{app}') debug evaluation. -Fix CreateRegistryEntries's SubKey logging. Was logging value before expansion. -Fix RmGetList error check. -Fix issue with duplicated files when the first file entry does not specify hash or issig verification, but the second does. -Add support for archives with more than 65535 (=$FFFF) items + fix comment typo. -Fix InputSignTool returning True when the command prompt query is canceled. -Fix TArchiveExtractAllCallback.Destroy memory leak. -Fix checkbox hot tracking on RTL + WantTabs False. Compiler IDE: -Find in Files, Replace All, and Select All Find Matches could enter an infinite loop, or miss a valid match at the end of the text. -Fix UninstallLogMode auto completion. -Fix UpdateLineNumbersWidth not being called for SCN_MODIFIED with deleted lines
Although there have been many more user facing bugs fixed recently, these should be all the more important ones.

Components/BitmapButton.pas
@@ -114,7 +114,7 @@ end;
 
 function TBitmapButton.InitializeFromIcon(const Instance: HINST; const Name: PChar; const BkColor: TColor; const AscendingTrySizes: array of Integer): Boolean;
 begin
-  Result := FImpl.InitializeFromIcon(HInstance, Name, BkColor, AscendingTrySizes);
+  Result := FImpl.InitializeFromIcon(Instance, Name, BkColor, AscendingTrySizes);
 end;
 
 function TBitmapButton.InitializeFromStockIcon(const Siid: SHSTOCKICONID; const BkColor: TColor; const AscendingTrySizes: array of Integer): Boolean;

Components/BitmapImage.pas
@@ -459,7 +459,7 @@ end;
 
 function TBitmapImage.InitializeFromIcon(const Instance: HINST; const Name: PChar; const BkColor: TColor; const AscendingTrySizes: array of Integer): Boolean;
 begin
-  Result := FImpl.InitializeFromIcon(HInstance, Name, BkColor, AscendingTrySizes);
+  Result := FImpl.InitializeFromIcon(Instance, Name, BkColor, AscendingTrySizes);
 end;
 
 function TBitmapImage.InitializeFromStockIcon(const Siid: SHSTOCKICONID; const BkColor: TColor; const AscendingTrySizes: array of Integer): Boolean;

Components/NewCheckListBox.pas
@@ -1864,9 +1864,16 @@ begin
   begin
     Rect := ItemRect(Index);
     Indent := (FOffset * 2 + FCheckWidth);
-    if FWantTabs or ((Pos.X >= Rect.Left + Indent * ItemLevel[Index]) and
-      (Pos.X < Rect.Left + Indent * (ItemLevel[Index] + 1))) then
-      NewHotIndex := Index;
+    if FWantTabs then
+      NewHotIndex := Index
+    else begin
+      var CheckRect := Rect;
+      CheckRect.Left := Rect.Left + Indent * ItemLevel[Index];
+      CheckRect.Right := CheckRect.Left + Indent;
+      FlipRect(CheckRect, ClientRect, IsRightToLeft);
+      if (Pos.X >= CheckRect.Left) and (Pos.X < CheckRect.Right) then
+        NewHotIndex := Index;
+    end;
   end;
   UpdateHotIndex(NewHotIndex);
 end;

Components/ScintEdit.pas
@@ -1557,7 +1557,7 @@ begin
         else if N.modificationType and SC_MOD_DELETETEXT <> 0 then
           Change(False, N.position, N.length, N.linesAdded);
 
-        if (N.linesAdded > 0) and FLineNumbers then
+        if (N.linesAdded <> 0) and FLineNumbers then
           UpdateLineNumbersWidth;
       end;
     SCN_SAVEPOINTLEFT,
@@ -1916,7 +1916,7 @@ begin
   CheckPosRange(StartPos, EndPos);
   Call(SCI_SETINDICATORCURRENT, IndicatorNumber, 0);
   if Value then begin
-    Call(SCI_SETINDICATORVALUE, IndicatorNumber, 1);
+    Call(SCI_SETINDICATORVALUE, 1, 0);
     Call(SCI_INDICATORFILLRANGE, StartPos, EndPos - StartPos);
   end else
     Call(SCI_INDICATORCLEARRANGE, StartPos, EndPos - StartPos);
@@ -2513,7 +2513,7 @@ begin
     if AutoCompleteFontName <> '' then
       DefaultAttr.FontName := AutoCompleteFontName;
     if AutoCompleteFontSize > 0 then
-    DefaultAttr.FontSize := AutoCompleteFontSize;
+      DefaultAttr.FontSize := AutoCompleteFontSize;
     DefaultAttr.FontStyle := [];
     { Note: Scintilla doesn't actually use the colors set here }
     DefaultAttr.ForeColor := clWindowText;

Projects/Src/Compiler.SetupCompiler.pas
@@ -5222,7 +5222,7 @@ type
           NewFileLocationEntryExtraInfo^.Verification.Hash := NewFileEntry^.Verification.Hash;
           NewFileLocationEntryExtraInfo^.Verification.ISSigAllowedKeys := NewFileEntry^.Verification.ISSigAllowedKeys;
         end else begin
-          { Verification.Typ changes checked below }
+          { Verification.Typ changes checked below using ApplyNewVerificationType }
           if (NewFileLocationEntryExtraInfo^.Verification.Typ = fvHash) and
              (NewFileEntry^.Verification.Typ = fvHash) and
              not CompareMem(@NewFileLocationEntryExtraInfo^.Verification.Hash[0],
@@ -5232,6 +5232,10 @@ type
              (NewFileEntry^.Verification.Typ = fvISSig) and
              (NewFileLocationEntryExtraInfo^.Verification.ISSigAllowedKeys <> NewFileEntry^.Verification.ISSigAllowedKeys) then
             AbortCompileFmt(SCompilerFilesValueConflict, ['ISSigAllowedKeys']);
+          if NewFileLocationEntryExtraInfo^.Verification.Typ = fvNone then begin
+            NewFileLocationEntryExtraInfo^.Verification.Hash := NewFileEntry^.Verification.Hash;
+            NewFileLocationEntryExtraInfo^.Verification.ISSigAllowedKeys := NewFileEntry^.Verification.ISSigAllowedKeys;
+          end;
         end;
         if Touch then
           Include(NewFileLocationEntryExtraInfo^.Flags, floTouch);

Projects/Src/Compression.SevenZipDLLDecoder.pas
@@ -231,11 +231,11 @@ type
 function GetProperty(const InArchive: IInArchive; const index: UInt32;
   const propID: PROPID; const allowedTypes: TVarTypeSet; out value: OleVariant): Boolean; overload;
 { Raises an EOleSysError exception on error but otherwise always sets value,
-  returning True if it's not empty. Set index to $FFFF to query an archive property
-  instead of an item propery }
+  returning True if it's not empty. Set index to $FFFFFFFF to query an archive
+  property instead of an item property }
 begin
   var Res: HRESULT;
-  if index = $FFFF then
+  if index = $FFFFFFFF then
     Res := InArchive.GetArchiveProperty(propID, value)
   else
     Res := InArchive.GetProperty(index, propID, value);
@@ -714,6 +714,7 @@ end;
 destructor TArchiveExtractAllCallback.Destroy;
 begin
   FLogQueue.Free;
+  inherited;
 end;
 
 function TArchiveExtractAllCallback.GetIndices: TArchiveExtractBaseCallback.TArrayOfUInt32;
@@ -1031,7 +1032,7 @@ begin
       Give up trying to get or open it on any error }
     var MainSubFile: Cardinal;
     var SubSeqStream: ISequentialInStream;
-    if not GetProperty(Result, $FFFF, kpidMainSubfile, MainSubFile) or
+    if not GetProperty(Result, $FFFFFFFF, kpidMainSubfile, MainSubFile) or
        (MainSubFile <> 0) or
        not Supports(Result, IInArchiveGetStream) or
        ((Result as IInArchiveGetStream).GetStream(MainSubFile, SubSeqStream) <> S_OK) or
@@ -1190,16 +1191,14 @@ begin
     LogFmt('Start extracting archive %s to %s. Recurse subdirs? %s', [ArchiveFilename,
       RemoveBackslashUnlessRoot(DestDir), SYesNo[RecurseSubDirs]]);
     var Solid: Boolean;
-    if GetProperty(State.InArchive, $FFFF, kpidSolid, Solid) and Solid then
+    if GetProperty(State.InArchive, $FFFFFFFF, kpidSolid, Solid) and Solid then
       Log('Archive is solid; extraction performance may degrade');
   end;
 
   if State.numItems > 0 then begin
     for var currentIndex: UInt32 := 0 to State.numItems-1 do begin
+      State.currentIndex := currentIndex;
       if State.GetInitialCurrentFindData(FindFileData) then begin
-        { Finish state }
-        State.currentIndex := currentIndex;
-
         { Save state }
         if ArchiveFindStates = nil then
           ArchiveFindStates := TArchiveFindStates.Create;

Projects/Src/IDE.MainForm.FindReplaceHelper.pas
@@ -219,8 +219,7 @@ begin
       var EndPos := Memo.RawTextLength;
       var FileHits := 0;
       var Range: TScintRange;
-      while (StartPos < EndPos) and
-            Memo.FindText(StartPos, EndPos, FLastFindText,
+      while Memo.FindText(StartPos, EndPos, FLastFindText,
               FindOptionsToSearchOptions(FLastFindOptions, FLastFindRegEx), Range) do begin
         { Also see UpdateFindResult }
         var Line := Memo.GetLineFromPosition(Range.StartPos);
@@ -235,6 +234,12 @@ begin
         FindResultsList.Items.AddObject(Prefix + Memo.Lines[Line], FindResult);
         Inc(FileHits);
         StartPos := Range.EndPos;
+        { If the match was empty the next search would match at the same position, so must manually progress }
+        if Range.Empty then begin
+          if StartPos = EndPos then
+            Break;
+          StartPos := Memo.GetPositionAfter(StartPos);
+        end;
       end;
       Inc(Files);
       if FileHits > 0 then begin
@@ -332,12 +337,20 @@ begin
     var ReplaceCount := 0;
     FActiveMemo.BeginUndoAction;
     try
-      var Pos := 0;
+      var StartPos := 0;
+      var EndPos := FActiveMemo.RawTextLength;
       var Range: TScintRange;
-      while FActiveMemo.FindText(Pos, FActiveMemo.RawTextLength, FLastFindText,
-         FindOptionsToSearchOptions(FLastFindOptions, FLastFindRegEx), Range) do begin
+      while FActiveMemo.FindText(StartPos, EndPos, FLastFindText,
+              FindOptionsToSearchOptions(FLastFindOptions, FLastFindRegEx), Range) do begin
         var NewRange := FActiveMemo.ReplaceTextRange(Range.StartPos, Range.EndPos, FLastReplaceText, ReplaceMode);
-        Pos := NewRange.EndPos;
+        StartPos := NewRange.EndPos;
+        EndPos := FActiveMemo.RawTextLength; { Update for replacement }
+        { If the match was empty there's no consumed text guaranteeing progress past it, so must manually progress }
+        if Range.Empty then begin
+          if StartPos = EndPos then
+            Break;
+          StartPos := FActiveMemo.GetPositionAfter(StartPos);
+        end;
         Inc(ReplaceCount);
       end;
     finally

Projects/Src/IDE.MainForm.ScintHelper.pas
@@ -2,7 +2,7 @@ unit IDE.MainForm.ScintHelper;
 
 {
   Inno Setup
-  Copyright (C) 1997-2025 Jordan Russell
+  Copyright (C) 1997-2026 Jordan Russell
   Portions by Martijn Laan
   For conditions of distribution and use, see LICENSE.TXT.
 
@@ -361,8 +361,7 @@ begin
   var ClosestSelectionDistance := 0; { Silence compiler }
   var CaretPos := AMemo.CaretPosition;
 
-  while (StartPos < EndPos) and
-        AMemo.FindText(StartPos, EndPos, FLastFindText,
+  while AMemo.FindText(StartPos, EndPos, FLastFindText,
           FindOptionsToSearchOptions(FLastFindOptions, FLastFindRegEx), FoundRange) do begin
     if StartPos = 0 then
       AMemo.SetSingleSelection(FoundRange.EndPos, FoundRange.StartPos)
@@ -376,6 +375,12 @@ begin
     end;
 
     StartPos := FoundRange.EndPos;
+    { If the match was empty the next search would match at the same position, so must manually progress }
+    if FoundRange.Empty then begin
+      if StartPos = EndPos then
+        Break;
+      StartPos := AMemo.GetPositionAfter(StartPos);
+    end;
   end;
   if ClosestSelection <> -1 then begin
     AMemo.MainSelection := ClosestSelection;

Projects/Src/IDE.MainForm.pas
@@ -4804,7 +4804,7 @@ begin
     if ConstRange.EndPos > ConstRange.StartPos then begin
       var HintStr := FActiveMemo.GetTextRange(ConstRange.StartPos, ConstRange.EndPos);
       var Output: String;
-      case EvaluateConstant(Info.HintStr, Output) of
+      case EvaluateConstant(HintStr, Output) of
         1: HintStr := HintStr + ' = "' + Output + '"';
         2: HintStr := HintStr + ' = Exception: ' + Output;
       else

Projects/Src/IDE.ScintStylerInnoSetup.pas
@@ -2005,7 +2005,7 @@ initialization
     SSDV(ssLZMAUseSeparateProcess, ['x86', SYes, SNo]),
     SSDV(ssPrivilegesRequired, ['admin', 'lowest']), { We don't list none/poweruser }
     SSDV(ssPrivilegesRequiredOverridesAllowed, ['commandline', 'dialog']),
-    SSDV(ssUninstallLogMode, ['append', 'new', 'override']),
+    SSDV(ssUninstallLogMode, ['append', 'new', 'overwrite']),
     SSDV(ssUseSetupLdr, ['x86', 'x64', SYes, SNo]),
     SSDV(ssWizardImageAlphaFormat, ['none', 'defined', 'premultiplied']),
     SSDV(ssWizardStyle, ['classic', 'modern', 'light', 'dark', 'dynamic', 'excludelightbuttons', 'excludelightcontrols', 'includetitlebar', 'hidebevels', 'polar', 'slate', 'windows11', 'zircon'])];

Projects/Src/IDE.SignToolsForm.pas
@@ -133,9 +133,9 @@ begin
         MsgBox('Invalid command.', Caption, mbCriticalError, MB_OK);
         Exit;
       end;
+      
+      Result := True;
     end;
-
-    Result := True;
   end;
 end;
 

Projects/Src/ISPP.Funcs.pas
@@ -199,7 +199,7 @@ begin
   if CheckParams(Params, [evStr, evStr, evStr, evStr], 3, Result) then
   try
     with IInternalFuncParams(Params) do
-      with TIniFile.Create(Get(0).AsStr) do
+      with TIniFile.Create(PrependPath(Ext, Get(0).AsStr)) do
       try
         if GetCount < 4 then Default := '' else Default := Get(3).AsStr;
         MakeStr(ResPtr^, ReadString(Get(1).AsStr, Get(2).AsStr, Default));
@@ -221,7 +221,7 @@ begin
   if CheckParams(Params, [evStr, evStr, evStr, evSpecial], 4, Result) then
   try
     with IInternalFuncParams(Params) do
-      with TIniFile.Create(Get(0).AsStr) do
+      with TIniFile.Create(PrependPath(Ext, Get(0).AsStr)) do
       try
         case Get(3).Typ of
           evInt: WriteInt64(Get(1).AsStr, Get(2).AsStr, Get(3).AsInt64);
@@ -1999,7 +1999,7 @@ begin
   try
     with IInternalFuncParams(Params) do
     begin
-      MakeBool(ResPtr^, Is64BitPEImage(Get(0).AsStr));
+      MakeBool(ResPtr^, Is64BitPEImage(PrependPath(Ext, Get(0).AsStr)));
     end;
   except
     on E: Exception do

Projects/Src/Setup.Install.pas
@@ -1986,7 +1986,7 @@ begin
               end;
             end;
           end;
-          if not Skip then
+          if not Skip then begin
             Log('Updating the .INI file.');
             repeat
               if SetIniString(IniSection, IniEntry, IniValue, IniFilename) then begin
@@ -1996,6 +1996,7 @@ begin
              until AbortRetryIgnoreTaskDialogMsgBox(
                      FmtSetupMessage1(msgErrorIniEntry, IniFilename),
                      [SetupMessages[msgAbortRetryIgnoreRetry], SetupMessages[msgAbortRetryIgnoreIgnore], SetupMessages[msgAbortRetryIgnoreCancel]]);
+          end;
         end else
           Log('Skipping updating the .INI file, only updating uninstall log.');
 
@@ -2082,7 +2083,7 @@ begin
         if RK = HKEY_AUTO then
           RK := InstallModeRootKey;
         S := ExpandConst(Subkey);
-        LogFmt('Key: %s\%s', [GetRegRootKeyName(RK), Subkey]);
+        LogFmt('Key: %s\%s', [GetRegRootKeyName(RK), S]);
         N := ExpandConst(ValueName);
         if N <> '' then
           LogFmt('Value name: %s', [N]);

Projects/Src/Setup.WizardForm.pas
@@ -2072,7 +2072,7 @@ begin
           GetMem(ProcessInfos, ProcessInfosCountNeeded * SizeOf(ProcessInfos[0]));
           ProcessInfosCount := ProcessInfosCountNeeded;
 
-          if not RmGetList(RmSessionHandle, @ProcessInfosCountNeeded, @ProcessInfosCount, ProcessInfos, @RebootReasons) in [ERROR_SUCCESS, ERROR_MORE_DATA] then begin
+          if not (RmGetList(RmSessionHandle, @ProcessInfosCountNeeded, @ProcessInfosCount, ProcessInfos, @RebootReasons) in [ERROR_SUCCESS, ERROR_MORE_DATA]) then begin
             RmEndSession(RmSessionHandle);
             RmSessionStarted := False;
             Break;

whatsnew.htm
@@ -48,6 +48,21 @@ For conditions of distribution and use, see <a href="files/is/license.txt">LICEN
 <details open="">
 <summary>Inno Setup 6.7</summary>
 
+<p><a name="6.7.2"></a><span class="ver">6.7.2-dev </span><span class="date">(?)</span></p>
+
+<ul>
+  <li>Fixes backported from Inno Setup 7:
+  <ul>
+    <li>Compiler IDE: <i>Find in Files</i>, <i>Replace All</i>, and <i>Select All Find Matches</i> could enter an infinite loop, or miss a valid match at the end of the text, when using a regular expression that matches zero-length text (for example <tt>^</tt> or <tt>\b</tt>).</li>
+    <li><tt>[Files]</tt> section entries with the <tt>extractarchive</tt> flag could fail to extract any files from an archive if the first entry in the archive didn't match.<br/>
+        For example, if the archive contained a file in a subdirectory as its first entry (for example, <tt>subdir\file1.txt</tt> followed by <tt>file2.txt</tt>) and <tt>recursesubdirs</tt> was not used, no files would be extracted at all, not even <tt>file2.txt</tt>.</li>
+    <li>ISPP: Fixed handling of relative filenames by support functions <tt>ReadIni</tt>, <tt>WriteIni</tt>, and <tt>Is64BitPEImage</tt> to be the same as other file-related support functions.</li>
+    <li>Pascal Scripting: Fixed support functions <tt>InitializeBitmapButtonFromIcon</tt> (broken since its introduction) and <tt>InitializeBitmapImageFromIcon</tt> (broken since 6.5.0).</li>
+    <li>Other minor fixes.</li>
+  </ul>
+  </li>
+</ul>
+
 <p><a name="6.7.1"></a><span class="ver">6.7.1 </span><span class="date">(2026-02-17)</span></p>
 
 <span class="head2">General improvements</span>