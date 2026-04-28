c034404f8aaa2d16954b65c2a263e1265ed46e77
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-04-28 19:41:06 +0200

    ISCC: Prevent it from always creating our registry key, even though its only reading from it.
    Using TRegistry to keep it close to TRegIniFile's actual implementation.
    
    Closes #592.

--- Projects/Src/Shared.ConfigIniFile.pas
+++ Projects/Src/Shared.ConfigIniFile.pas
@@ -2,7 +2,7 @@ unit Shared.ConfigIniFile;
 
 {
   Inno Setup
-  Copyright (C) 1997-2024 Jordan Russell
+  Copyright (C) 1997-2026 Jordan Russell
   Portions by Martijn Laan
   For conditions of distribution and use, see LICENSE.TXT.
 
@@ -22,15 +22,19 @@ type
   public
     constructor Create;
     destructor Destroy; override;
+    class function Exists: Boolean; static;
   end;
 
 implementation
 
 { TConfigIniFile }
 
+const
+  SubKeyName = 'Software\Jordan Russell\Inno Setup';
+
 constructor TConfigIniFile.Create;
 begin
-  inherited Create('Software\Jordan Russell\Inno Setup');
+  inherited Create(SubKeyName); { This always creates the key }
   { Paranoia: Use a mutex to prevent multiple instances from reading/writing
     to the registry simultaneously }
   FMutex := CreateMutex(nil, False, 'Inno-Setup-IDE-Config-Mutex');
@@ -47,6 +51,18 @@ begin
     CloseHandle(FMutex);
   end;
   inherited;
+end;
+
+{ When reading, use this function before calling Create to
+  prevent it from always creating the key }
+class function TConfigIniFile.Exists: Boolean;
+begin
+  const Registry = TRegistry.Create;
+  try
+    Result := Registry.OpenKeyReadOnly(SubKeyName);
+  finally
+    Registry.Free;
+  end;
 end;
 
 end.
