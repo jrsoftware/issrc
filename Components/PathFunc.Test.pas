ea3848a20776176d34238d117d0203e1ab2c87fc
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-04-29 17:08:11 +0200

    Add more PathFunc tests.

--- Components/PathFunc.Test.pas
+++ Components/PathFunc.Test.pas
@@ -154,6 +154,155 @@ procedure PathFuncRunTests;
       raise Exception.Create('PathStrFind test failed');
   end;
 
+  procedure TestAddBackslash(const S, ExpectedResult: String);
+  begin
+    if AddBackslash(S) <> ExpectedResult then
+      raise Exception.Create('AddBackslash test failed');
+  end;
+
+  procedure TestPathSame(const S1, S2: String; const ExpectedResult: Boolean);
+  begin
+    if PathSame(S1, S2) <> ExpectedResult then
+      raise Exception.Create('PathSame test failed');
+  end;
+
+  procedure TestPathCompare(const S1, S2: String; const IgnoreCase: Boolean;
+    const ExpectedSign: Integer);
+  begin
+    if CompareResultSign(PathCompare(S1, S2, IgnoreCase)) <> ExpectedSign then
+      raise Exception.Create('PathCompare test failed');
+  end;
+
+  procedure TestPathCompareDefault(const S1, S2: String; const ExpectedSign: Integer);
+  begin
+    if CompareResultSign(PathCompare(S1, S2)) <> ExpectedSign then
+      raise Exception.Create('PathCompare test failed');
+  end;
+
+  procedure TestPathExtracts(const Filename, ExpectedName, ExpectedPath, ExpectedDir, ExpectedDrive: String);
+  begin
+    if PathExtractName(Filename) <> ExpectedName then
+      raise Exception.Create('PathExtractName test failed');
+    if PathExtractPath(Filename) <> ExpectedPath then
+      raise Exception.Create('PathExtractPath test failed');
+    if PathExtractDir(Filename) <> ExpectedDir then
+      raise Exception.Create('PathExtractDir test failed');
+    if PathExtractDrive(Filename) <> ExpectedDrive then
+      raise Exception.Create('PathExtractDrive test failed');
+  end;
+
+  procedure TestPathExtensionPos(const Filename: String; const ExpectedResult: Integer);
+  begin
+    if PathExtensionPos(Filename) <> ExpectedResult then
+      raise Exception.Create('PathExtensionPos test failed');
+  end;
+
+  procedure TestPathHasSubstringAt(const S, Substring: String; const Offset: Integer;
+    const ExpectedResult: Boolean);
+  begin
+    if PathHasSubstringAt(S, Substring, Offset) <> ExpectedResult then
+      raise Exception.Create('PathHasSubstringAt test failed');
+  end;
+
+  procedure TestPathHasInvalidCharacters(const S: String;
+    const AllowDriveLetterColon, ExpectedResult: Boolean);
+  begin
+    if PathHasInvalidCharacters(S, AllowDriveLetterColon) <> ExpectedResult then
+      raise Exception.Create('PathHasInvalidCharacters test failed');
+  end;
+
+  procedure TestPathComponentIsReservedName(const SingleComponent: String;
+    const ExpectedResult: Boolean);
+  begin
+    if PathComponentIsReservedName(SingleComponent) <> ExpectedResult then
+      raise Exception.Create('PathComponentIsReservedName test failed');
+  end;
+
+  procedure TestValidateAndCombinePath(const ADestDir, AFilename, ExpectedResultingPath: String;
+    const ExpectedResult: Boolean);
+  begin
+    var ResultingPath: String;
+    if ValidateAndCombinePath(ADestDir, AFilename, ResultingPath) <> ExpectedResult then
+      raise Exception.Create('ValidateAndCombinePath test failed');
+    if ExpectedResult then begin
+      if ResultingPath <> ExpectedResultingPath then
+        raise Exception.Create('ValidateAndCombinePath test failed');
+    end else begin
+      { Failure should not expose a partial path. Since AResultingPath is an out
+        string, this cannot distinguish no assignment from assigning ''. }
+      if ResultingPath <> '' then
+        raise Exception.Create('ValidateAndCombinePath test failed');
+    end;
+    if ValidateAndCombinePath(ADestDir, AFilename) <> ExpectedResult then
+      raise Exception.Create('ValidateAndCombinePath test failed');
+  end;
+
+  procedure TestPathConvertNormalToSuper(const Filename, ExpectedSuper: String;
+    const ExpectedResult: Boolean);
+  begin
+    var SuperFilename: String;
+    if PathConvertNormalToSuper(Filename, SuperFilename) <> ExpectedResult then
+      raise Exception.Create('PathConvertNormalToSuper test failed');
+    if ExpectedResult and (SuperFilename <> ExpectedSuper) then
+      raise Exception.Create('PathConvertNormalToSuper test failed');
+  end;
+
+  procedure TestPathConvertSuperToNormal(const Filename, ExpectedResult: String);
+  begin
+    if PathConvertSuperToNormal(Filename) <> ExpectedResult then
+      raise Exception.Create('PathConvertSuperToNormal test failed');
+  end;
+
+  procedure TestPathLastDelimiter(const Delimiters, S: String; const ExpectedResult: Integer);
+  begin
+    if PathLastDelimiter(Delimiters, S) <> ExpectedResult then
+      raise Exception.Create('PathLastDelimiter test failed');
+  end;
+
+  procedure TestPathLowercase(const S, ExpectedResult: String);
+  begin
+    if PathLowercase(S) <> ExpectedResult then
+      raise Exception.Create('PathLowercase test failed');
+  end;
+
+  procedure TestPathPos(const C: Char; const S: String;
+    const Offset, ExpectedResult: Integer);
+  begin
+    if PathPos(C, S, Offset) <> ExpectedResult then
+      raise Exception.Create('PathPos test failed');
+  end;
+
+  procedure TestPathLastChar(const S: String; const ExpectedChar: Char);
+  begin
+    const Ptr = PathLastChar(S);
+    if (Ptr = nil) or (Ptr^ <> ExpectedChar) then
+      raise Exception.Create('PathLastChar test failed');
+  end;
+
+  procedure TestPathLastCharNil(const S: String);
+  begin
+    if PathLastChar(S) <> nil then
+      raise Exception.Create('PathLastChar test failed');
+  end;
+
+  procedure TestPathCharIsSlash(const C: Char; const ExpectedResult: Boolean);
+  begin
+    if PathCharIsSlash(C) <> ExpectedResult then
+      raise Exception.Create('PathCharIsSlash test failed');
+  end;
+
+  procedure TestPathCharIsDriveLetter(const C: Char; const ExpectedResult: Boolean);
+  begin
+    if PathCharIsDriveLetter(C) <> ExpectedResult then
+      raise Exception.Create('PathCharIsDriveLetter test failed');
+  end;
+
+  procedure TestPathNormalizeSlashes(const S, ExpectedResult: String);
+  begin
+    if PathNormalizeSlashes(S) <> ExpectedResult then
+      raise Exception.Create('PathNormalizeSlashes test failed');
+  end;
+
 begin
   { * = Bogus path case. What the "correct" result should be is debatable. }
   { ** = Possible access to NTFS alternate data stream. The characters before
@@ -303,6 +452,11 @@ begin
   TestPathEndsWith(True, 'TestingAbc', 'testingABC', True);
   TestPathEndsWith(True, 'TestingAbc', 'xTestingAbc', False);
 
+  { Edge cases not covered through PathStartsWith/PathEndsWith }
+  TestPathHasSubstringAt('abc', 'a', -1, False);
+  TestPathHasSubstringAt('abc', '', 3, True);
+  TestPathHasSubstringAt('abc', 'a', 99, False);
+
   TestPathExpand('', '', False);
   TestPathExpand(' ', '', False);
   TestPathExpand('   ', '', False);
@@ -342,6 +496,174 @@ begin
   TestPathStrFind('abcABC', 'xyz', True, -1);
   TestPathStrFind('abc'+#0+'ABC', 'ABC', False, -1, True);
   TestPathStrFind('abc'+#0+'ABC', 'ABC', False, 4);
+
+  TestAddBackslash('', '');
+  TestAddBackslash('a', 'a\');
+  TestAddBackslash('a\', 'a\');
+  TestAddBackslash('a/', 'a/');
+
+  TestPathSame('abc', 'abc', True);
+  TestPathSame('abc', 'ABC', True);
+  TestPathSame('abc', 'abcd', False);
+  TestPathSame('', '', True);
+  TestPathSame('', 'a', False);
+  { Super path and its normal version are not equivalent }
+  TestPathSame('C:\x', '\\?\C:\x', False);
+
+  TestPathCompare('abc', 'ABC', True, 0);
+  TestPathCompareDefault('abc', 'ABC', 0);
+  TestPathCompare('abc', 'ABC', False, 1);
+  TestPathCompare('abc', 'abd', True, -1);
+  TestPathCompare('abd', 'abc', True, 1);
+  TestPathCompare('abc'+#0+'def', 'abc'+#0+'def', True, 0);
+  TestPathCompare('abc'+#0+'def', 'abc', True, 1);
+
+  TestPathExtracts('c:\dir\file.txt', 'file.txt', 'c:\dir\', 'c:\dir', 'c:');
+  TestPathExtracts('c:\dir\', '', 'c:\dir\', 'c:\dir', 'c:');
+  TestPathExtracts('c:\', '', 'c:\', 'c:\', 'c:');
+  TestPathExtracts('C:file', 'file', 'C:', 'C:', 'C:');
+  TestPathExtracts('\\s\sh\file', 'file', '\\s\sh\', '\\s\sh', '\\s\sh');
+  TestPathExtracts('\\s\sh', '', '\\s\sh', '\\s\sh', '\\s\sh');
+  TestPathExtracts('\\?\C:\x', 'x', '\\?\C:\', '\\?\C:\', '\\?\C:');
+  TestPathExtracts('\\.\C:\x', 'x', '\\.\C:\', '\\.\C:\', '\\.\C:');
+  TestPathExtracts('file.txt', 'file.txt', '', '', '');
+  TestPathExtracts('\file', 'file', '\', '\', '');
+  TestPathExtracts('', '', '', '', '');
+  TestPathExtracts('a\b:c', 'b:c', 'a\', 'a', ''); {**}
+
+  TestPathExtensionPos('', 0);
+  TestPathExtensionPos('file', 0);
+  TestPathExtensionPos('file.txt', 5);
+  TestPathExtensionPos('a.b.c', 4); { Last dot wins }
+  TestPathExtensionPos('c:\dir.x\a', 0); { Dot in path part is ignored }
+  TestPathExtensionPos('c:\dir.x\a.txt', 11);
+  TestPathExtensionPos('.hidden', 1); { Leading dot is the extension }
+  TestPathExtensionPos('a.', 2);
+
+  TestPathHasInvalidCharacters('', False, False);
+  TestPathHasInvalidCharacters('', True, False);
+  TestPathHasInvalidCharacters('file.txt', False, False);
+  TestPathHasInvalidCharacters('C:\dir\file', True, False);
+  TestPathHasInvalidCharacters('\\server\share\a', False, False);
+  TestPathHasInvalidCharacters('C:', True, False);
+  TestPathHasInvalidCharacters('C:\dir', True, False);
+
+  TestPathHasInvalidCharacters('C:\dir\file', False, True);
+  TestPathHasInvalidCharacters('a'+#1+'b', False, True);
+  TestPathHasInvalidCharacters('a/b', False, True);
+  TestPathHasInvalidCharacters('a*b', False, True);
+  TestPathHasInvalidCharacters('a?b', False, True);
+  TestPathHasInvalidCharacters('a"b', False, True);
+  TestPathHasInvalidCharacters('a<b', False, True);
+  TestPathHasInvalidCharacters('a>b', False, True);
+  TestPathHasInvalidCharacters('a|b', False, True);
+  TestPathHasInvalidCharacters('\\?\C:\x', False, True);
+  TestPathHasInvalidCharacters('\??\C:\x', False, True);
+  TestPathHasInvalidCharacters('file.', False, True);
+  TestPathHasInvalidCharacters('file ', False, True);
+  TestPathHasInvalidCharacters('file. . .', False, True);
+  TestPathHasInvalidCharacters('file . . ', False, True);
+  TestPathHasInvalidCharacters('dir.\x', False, True);
+  TestPathHasInvalidCharacters('dir \x', False, True);
+  TestPathHasInvalidCharacters('.', False, True);
+  TestPathHasInvalidCharacters('..', False, True);
+  TestPathHasInvalidCharacters('C:', False, True);
+  TestPathHasInvalidCharacters('a:b', False, True);
+  TestPathHasInvalidCharacters('ab:', True, True);
+  TestPathHasInvalidCharacters('.:stream', True, True);
+  TestPathHasInvalidCharacters(' :stream', True, True);
+  TestPathHasInvalidCharacters('\:stream', True, True);
+  TestPathHasInvalidCharacters('C:\a:b', True, True);
+
+  TestPathComponentIsReservedName('', False);
+  TestPathComponentIsReservedName('CON', True);
+  TestPathComponentIsReservedName('Con', True);
+  TestPathComponentIsReservedName('CONSOLE', False);
+  TestPathComponentIsReservedName('PRN', True);
+  TestPathComponentIsReservedName('AUX', True);
+  TestPathComponentIsReservedName('NUL', True);
+  TestPathComponentIsReservedName('NULL', False);
+  TestPathComponentIsReservedName('NUL ', False);
+  TestPathComponentIsReservedName('NUL:', False);
+  TestPathComponentIsReservedName('COM0', False);
+  TestPathComponentIsReservedName('COM1', True);
+  TestPathComponentIsReservedName('COM9', True);
+  TestPathComponentIsReservedName('COM'+#$00B9, True);
+  TestPathComponentIsReservedName('COM'+#$00B2, True);
+  TestPathComponentIsReservedName('COM'+#$00B3, True);
+  TestPathComponentIsReservedName('COM10', False);
+  TestPathComponentIsReservedName('LPT1', True);
+  TestPathComponentIsReservedName('LPT9', True);
+  TestPathComponentIsReservedName('LPT'+#$00B9, True);
+  TestPathComponentIsReservedName('LPTA', False);
+  TestPathComponentIsReservedName('NUL.txt', True);
+  TestPathComponentIsReservedName('COM1.anything', True);
+
+  TestValidateAndCombinePath('c:\dest\', 'sub\file.txt', 'c:\dest\sub\file.txt', True); { success }
+  TestValidateAndCombinePath('c:\dest\', '', '', False);                                { empty }
+  TestValidateAndCombinePath('c:\dest\', 'c:\file', '', False);                         { rooted }
+  TestValidateAndCombinePath('c:\dest\', 'a/b', '', False);                             { not normalized }
+  TestValidateAndCombinePath('c:\dest\', 'sub\', '', False);                            { trailing slash }
+  TestValidateAndCombinePath('c:\dest\', '..\x', '', False);                            { invalid chars (incl. ..) }
+  TestValidateAndCombinePath('c:\dest\', 'sub\NUL', '', False);                         { reserved name }
+
+  TestPathConvertNormalToSuper('C:\dir\file', '\\?\C:\dir\file', True);
+  TestPathConvertNormalToSuper('C:', '\\?\' + PathExpand('C:'), True);
+  TestPathConvertNormalToSuper('\\server\share\x', '\\?\UNC\server\share\x', True);
+  TestPathConvertNormalToSuper('\\.\C:\x', '\\?\C:\x', True);
+  TestPathConvertNormalToSuper('\\?\C:\x', '\\?\C:\x', True);
+  TestPathConvertNormalToSuper('sub\file', '\\?\' + AddBackslash(GetCurrentDir) + 'sub\file', True);
+  TestPathConvertNormalToSuper('\\', '\\?\UNC\', True);
+  TestPathConvertNormalToSuper('', '', False);
+
+  TestPathConvertSuperToNormal('\\?\C:', 'C:\');
+  TestPathConvertSuperToNormal('\\?\C:\', 'C:\');
+  TestPathConvertSuperToNormal('\\?\C:\x', 'C:\x');
+  TestPathConvertSuperToNormal('\\?\UNC\s\sh', '\\s\sh');
+  TestPathConvertSuperToNormal('C:\x', 'C:\x');
+  TestPathConvertSuperToNormal('\\s\sh', '\\s\sh');
+  TestPathConvertSuperToNormal('', '');
+
+  TestPathLastDelimiter('\/', 'a\b/c', 4);
+  TestPathLastDelimiter('\/', 'abc', 0);
+  TestPathLastDelimiter('', 'abc\/', 0);
+  TestPathLastDelimiter('\/', '', 0);
+  TestPathLastDelimiter('', '', 0);
+  { Search continues past #0 in S; #0 in Delimiters ends the delimiter set }
+  TestPathLastDelimiter('\/', 'a'+#0+'\b'+#0+'/c', 6);
+  TestPathLastDelimiter('\'+#0+'/', 'a\b/c', 2);
+
+  TestPathLowercase('ABC', 'abc');
+  TestPathLowercase('abc', 'abc');
+  TestPathLowercase('', '');
+
+  TestPathPos('b', 'abc', 1, 2);
+  TestPathPos('b', 'bcb', 2, 3);
+  TestPathPos('z', 'abc', 1, 0);
+  TestPathPos('a', '', 1, 0);
+  { Bad offset }
+  TestPathPos('a', 'abc', 0, 0);
+  TestPathPos('a', 'abc', 99, 0);
+
+  TestPathLastChar('abc', 'c');
+  TestPathLastCharNil('');
+
+  TestPathCharIsSlash('\', True);
+  TestPathCharIsSlash('/', True);
+  TestPathCharIsSlash('a', False);
+  TestPathCharIsSlash(':', False);
+
+  TestPathCharIsDriveLetter('a', True);
+  TestPathCharIsDriveLetter('Z', True);
+  TestPathCharIsDriveLetter('1', False);
+
+  TestPathNormalizeSlashes('a/\b', 'a\b');
+  TestPathNormalizeSlashes('a\\b', 'a\b');
+  TestPathNormalizeSlashes('\\a\\\b', '\\a\b');
+  TestPathNormalizeSlashes('a/b', 'a\b');
+  TestPathNormalizeSlashes('a//b', 'a\b');
+  TestPathNormalizeSlashes('///', '\\\'); { 3+ leading slash quirk preserved }
+  TestPathNormalizeSlashes('', '');
 end;
 
 {$IFDEF DEBUG}
