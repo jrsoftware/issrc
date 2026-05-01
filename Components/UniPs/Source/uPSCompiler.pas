7b835617417ec67ba36506c584ff077f154eca4f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-05-01 08:43:07 +0200

    Update ROPS: Third ReadInteger fix: Pass minus sign to ReadInteger for negative integer literals.
    Previously, '-2147483648' was parsed in two separate steps: First '2147483648' would by read using ReadInteger and then later the '-' would be applied. But 2147483648 does not fit into a Integer and would become Cardinal (in ReadInteger). Negating Cardinal 2147483648 still gives 2147483648. So '-2147483648' was read as 2147483648.
    
    Likewise this fixes '-$80000000' to be correctly read as -2147483648 instead of 2147483648.

--- Components/UniPs/Source/uPSCompiler.pas
+++ Components/UniPs/Source/uPSCompiler.pas
@@ -5717,7 +5717,7 @@ begin
 end;
 
 
-{ Note: 's' never starts with a minus sign }
+{ Note: 's' may start with '-' when called from unary minus handling }
 function TPSPascalCompiler.ReadInteger(const s: tbtString): PIfRVariant;
 var
   R: {$IFNDEF PS_NOINT64}Int64;{$ELSE}Longint;{$ENDIF}
@@ -5731,7 +5731,7 @@ begin
   Val(string(s), r, Code);
   if Code = 0 then
   begin
-    { Even though 's' never starts with a minus sign, r will still be negative when '$8000000000000000' is used }
+    { r will be negative when 's' starts with '-' or when hex values like '$8000000000000000' are used }
     if (r >= Low(TbtS32)) and (r <= High(TbtS32)) then
     begin
       InitializeVariant(Result, at2ut(FindBaseType(bts32)));
@@ -8157,18 +8157,27 @@ function TPSPascalCompiler.ProcessSub(BlockInfo: TPSBlockInfo): Boolean;
         CSTI_Minus:
         begin
           FParser.Next;
-          NewVar := ReadTerm;
-          if NewVar = nil then
+          if FParser.CurrTokenId in [CSTI_HexInt, CSTI_Integer] then
           begin
-            Result := nil;
-            exit;
+            { See CSTI_HexInt, CSTI_Integer below }
+            NewVar := TPSValueData.Create;
+            NewVar.SetParserPos(FParser);
+            TPSValueData(NewVar).Data := ReadInteger(tbtString('-') + FParser.GetToken);
+            FParser.Next;
+          end else begin
+            NewVar := ReadTerm;
+            if NewVar = nil then
+            begin
+              Result := nil;
+              exit;
+            end;
+            NewVarU := TPSUnValueOp.Create;
+            NewVarU.SetParserPos(FParser);
+            NewVarU.aType := GetTypeNo(BlockInfo, NewVar);
+            NewVarU.Operator := otMinus;
+            NewVarU.Val1 := NewVar;
+            NewVar := NewVarU;
           end;
-          NewVarU := TPSUnValueOp.Create;
-          NewVarU.SetParserPos(FParser);
-          NewVarU.aType := GetTypeNo(BlockInfo, NewVar);
-          NewVarU.Operator := otMinus;
-          NewVarU.Val1 := NewVar;
-          NewVar := NewVarU;
         end;
         CSTII_Nil:
           begin
@@ -8237,6 +8246,7 @@ function TPSPascalCompiler.ProcessSub(BlockInfo: TPSBlockInfo): Boolean;
           end;
         CSTI_HexInt, CSTI_Integer:
           begin
+            { Also see CSTI_Minus above }
             NewVar := TPSValueData.Create;
             NewVar.SetParserPos(FParser);
             TPSValueData(NewVar).Data := ReadInteger(FParser.GetToken);
@@ -13125,16 +13135,25 @@ function TPSPascalCompiler.ReadConstant(FParser: TPSPascalParser; StopOn: TPSPas
       CSTI_Minus:
       begin
         FParser.Next;
-        NewVar := ReadTerm;
-        if NewVar = nil then
+        if FParser.CurrTokenId in [CSTI_HexInt, CSTI_Integer] then
         begin
-          Result := nil;
-          exit;
+          { See CSTI_HexInt, CSTI_Integer below }
+          NewVar := TConstData.Create;
+          NewVar.SetPos(FParser);
+          TConstData(NewVar).Data := ReadInteger(tbtString('-') + FParser.GetToken);
+          FParser.Next;
+        end else begin
+          NewVar := ReadTerm;
+          if NewVar = nil then
+          begin
+            Result := nil;
+            exit;
+          end;
+          NewVarU := TUnConstOperation.Create;
+          NewVarU.OpType := otMinus;
+          NewVarU.Val1 := NewVar;
+          NewVar := NewVarU;
         end;
-        NewVarU := TUnConstOperation.Create;
-        NewVarU.OpType := otMinus;
-        NewVarU.Val1 := NewVar;
-        NewVar := NewVarU;
       end;
       CSTI_OpenRound:
         begin
@@ -13162,6 +13181,7 @@ function TPSPascalCompiler.ReadConstant(FParser: TPSPascalParser; StopOn: TPSPas
         end;
       CSTI_HexInt, CSTI_Integer:
         begin
+          { Also see CSTI_Minus above }
           NewVar := TConstData.Create;
           NewVar.SetPos(FParser);
           TConstData(NewVar).Data := ReadInteger(FParser.GetToken);
