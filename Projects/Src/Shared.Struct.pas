777630094d67a8e937b4e77b50fe8f13734dc145
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-05-11 08:25:15 +0200

    is6: Prepare for release.

--- Projects/Src/Shared.Struct.pas
+++ Projects/Src/Shared.Struct.pas
@@ -17,8 +17,8 @@ uses
 
 const
   SetupTitle = 'Inno Setup';
-  SetupVersion = '6.7.1';
-  SetupBinVersion = (6 shl 24) + (7 shl 16) + (1 shl 8) + 0;
+  SetupVersion = '6.7.2';
+  SetupBinVersion = (6 shl 24) + (7 shl 16) + (2 shl 8) + 0;
 
 type
   TSetupID = array[0..63] of AnsiChar;
