bc0c4001ceb9a71542732381e7709bb98cf9884b
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-06-01 17:38:24 +0200

    IDE.LocalizeFunc: Add LocalizeComponent which can translate entire form at once. Not yet used, but works well when tested with MainForm and OptionsForm.
    Limitation: does not yet translate shortcut texts in menu items.
    
    VSCode has this when I set it to German (yes, it uses caps):
    -Shift => UMSCHALT
    -Ctrl => STRG
    -Alt => ALT
    At the same time it keeps things like 'RightArrow' untranslated.

--- Projects/Src/IDE.LocalizeFunc.pas
+++ Projects/Src/IDE.LocalizeFunc.pas
@@ -11,14 +11,19 @@ unit IDE.LocalizeFunc;
 
 interface
 
+uses
+  Classes;
+
 function LStr(const Str: String; const AllowEmpty: Boolean = False): String;
 function LStrFmt(const Str: String; const Args: array of const;
   const AllowEmpty: Boolean = False): String;
+procedure LocalizeComponent(const Component: TComponent);
 
 implementation
 
 uses
-  SysUtils;
+  SysUtils, Controls, StdCtrls, Menus,
+  NewTabSet;
 
 function LStr(const Str: String; const AllowEmpty: Boolean): String;
 begin
@@ -35,6 +40,52 @@ function LStrFmt(const Str: String; const Args: array of const;
   const AllowEmpty: Boolean): String;
 begin
   Result := Format(LStr(Str, AllowEmpty), Args);
+end;
+
+type
+  TControlAccess = class(TControl);
+
+procedure LocalizeComponent(const Component: TComponent);
+
+  procedure LocalizeStrings(const Strings: TStrings);
+  begin
+    Strings.BeginUpdate;
+    try
+      for var I := 0 to Strings.Count-1 do
+        if Strings[I] <> '' then
+          Strings[I] := LStr(Strings[I]);
+    finally
+      Strings.EndUpdate;
+    end;
+  end;
+
+begin
+  if Component is TControl then begin
+    const Control = TControl(Component);
+    if Control.Hint <> '' then
+      Control.Hint := LStr(Control.Hint);
+
+    const ControlAccess = TControlAccess(Control);
+    if ControlAccess.Text <> '' then { This is both Caption and Text }
+      ControlAccess.Text := LStr(ControlAccess.Text);
+
+    if Component is TComboBox then
+      LocalizeStrings(TComboBox(Component).Items)
+    else if Component is TNewTabSet then begin
+      LocalizeStrings(TNewTabSet(Component).Tabs);
+      LocalizeStrings(TNewTabSet(Component).Hints);
+    end else if Component is TCustomMemo then
+      LocalizeStrings(TCustomMemo(Component).Lines);
+  end else if Component is TMenuItem then begin
+    const MenuItem = TMenuItem(Component);
+    if MenuItem.Caption <> '' then
+      MenuItem.Caption := LStr(MenuItem.Caption);
+    if MenuItem.Hint <> '' then
+      MenuItem.Hint := LStr(MenuItem.Hint);
+  end;
+
+  for var I := 0 to Component.ComponentCount-1 do
+    LocalizeComponent(Component.Components[I]);
 end;
 
 end.
