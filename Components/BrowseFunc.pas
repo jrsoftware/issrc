jrsoftware/issrc

commit 70dc9baa954e037fa8c430ffd58432057d2b46b1

BrowseForFolder: Replace SHBrowseForFolder (deprecated and old look) with IFileDialog (new look).

Components/BrowseFunc.pas
@@ -12,7 +12,7 @@ unit BrowseFunc;
 interface
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms;
+  Windows, Classes, Forms;
 
 function BrowseForFolder(const Prompt: String; var Directory: String;
   const ParentWnd: HWND; const NewFolderButton: Boolean): Boolean;
@@ -29,79 +29,64 @@ function NewGetSaveFileName(const Prompt: String; var FileName: String;
 implementation
 
 uses
-  CommDlg, ShlObj, ActiveX, Themes,
+  CommDlg, ShlObj, ActiveX,
+  SysUtils, Themes,
   PathFunc;
-
-function BrowseCallback(Wnd: HWND; uMsg: UINT; lParam, lpData: LPARAM): Integer; stdcall;
-var
-  ShouldEnable: Boolean;
-  Path: array[0..MAX_PATH-1] of Char;
-begin
-  case uMsg of
-    BFFM_INITIALIZED:
-      begin
-        if lpData <> 0 then
-          SendMessage(Wnd, BFFM_SETSELECTION, 1, lpData);
-      end;
-    BFFM_SELCHANGED:
-      begin
-        { In a BIF_NEWDIALOGSTYLE dialog, BIF_RETURNONLYFSDIRS does not cause
-          the OK button to be disabled automatically when the user clicks a
-          non-FS item (e.g. My Computer), so do that ourself. }
-        ShouldEnable := SHGetPathFromIDList(PItemIDList(lParam), Path);
-        SendMessage(Wnd, BFFM_ENABLEOK, 0, Ord(ShouldEnable));
-      end;
-  end;
-  Result := 0;
-end;
 
 function BrowseForFolder(const Prompt: String; var Directory: String;
   const ParentWnd: HWND; const NewFolderButton: Boolean): Boolean;
-const
-  BIF_NONEWFOLDERBUTTON = $200;
-  BIF_NEWDIALOGSTYLE = $0040;
 var
   InitialDir: String;
-  Malloc: IMalloc;
-  BrowseInfo: TBrowseInfo;
-  DisplayName, Path: array[0..MAX_PATH-1] of Char;
+  FileDialog: IFileDialog;
+  Options: DWORD;
   ActiveWindow: HWND;
   WindowList: Pointer;
-  IDList: PItemIDList;
+  ShellItem: IShellItem;
+  ResultPath: PChar;
 begin
   Result := False;
   InitialDir := RemoveBackslashUnlessRoot(Directory);  { Win95 doesn't allow trailing backslash }
-  if FAILED(SHGetMalloc(Malloc)) then
-    Malloc := nil;
-  FillChar(BrowseInfo, SizeOf(BrowseInfo), 0);
-  with BrowseInfo do begin
-    hwndOwner := ParentWnd;
-    pszDisplayName := @DisplayName;
-    lpszTitle := PChar(Prompt);
-    ulFlags := BIF_RETURNONLYFSDIRS or BIF_NEWDIALOGSTYLE;
-    if not NewFolderButton then
-      ulFlags := ulFlags or BIF_NONEWFOLDERBUTTON;
-    lpfn := BrowseCallback;
-    if InitialDir <> '' then
-      Pointer(lParam) := PChar(InitialDir);
-  end;
+
+  if Failed(CoInitialize(nil)) then
+    Exit;
   ActiveWindow := GetActiveWindow;
   WindowList := DisableTaskWindows(ParentWnd);
-  CoInitialize(nil);
+  const SaveHooks = TStyleManager.SystemHooks;
+  TStyleManager.SystemHooks := []; { See below }
   try
-    IDList := SHBrowseForFolder(BrowseInfo);
+    if Failed(CoCreateInstance(CLSID_FileOpenDialog, nil, CLSCTX_INPROC_SERVER, IID_IFileDialog, FileDialog)) or
+       Failed(FileDialog.GetOptions(Options)) then { On Windows 11 this returned FOS_NOCHANGEDIR or FOS_PATHMUSTEXIST or FOS_FILEMUSTEXIST }
+      Exit;
+
+    Options := Options or FOS_PICKFOLDERS or FOS_FORCEFILESYSTEM;
+    if not NewFolderButton then begin
+      const FOS_NOCREATEFOLDERS: DWORD = $00000200;
+      Options := Options or FOS_NOCREATEFOLDERS;
+    end;
+    if Failed(FileDialog.SetOptions(Options)) then
+      Exit;
+
+    if Prompt <> '' then
+      FileDialog.SetTitle(PChar(Prompt));
+
+    if (InitialDir <> '') and Succeeded(SHCreateItemFromParsingName(PChar(InitialDir), nil, IID_IShellItem, ShellItem)) then
+      FileDialog.SetFolder(ShellItem);
+
+    if Failed(FileDialog.Show(ParentWnd)) or
+       Failed(FileDialog.GetResult(ShellItem)) or
+       Failed(ShellItem.GetDisplayName(SIGDN_FILESYSPATH, ResultPath)) then
+      Exit;
+
+    try
+      Directory := ResultPath;
+    finally
+      CoTaskMemFree(ResultPath);
+    end;
   finally
-    CoUninitialize();
+    TStyleManager.SystemHooks := SaveHooks;
     EnableTaskWindows(WindowList);
     SetActiveWindow(ActiveWindow);
-  end;
-  try
-    if (IDList = nil) or not SHGetPathFromIDList(IDList, Path) then
-      Exit;
-    Directory := Path;
-  finally
-    if Assigned(Malloc) then
-      Malloc.Free(IDList);
+    CoUninitialize();
   end;
   Result := True;
 end;

commit 399fb1de51f4a485e5147ab9d51fb993613dda4e

BrowseForFolder: Finish previous commit: still had to actually remove NewFolderButton.
By the way, the issue was introduced by 70dc9baa, in 6.6.0.

Not putting it in whatsnew, since I dont know of a nice way to explain the places this affects, and also 6.6.0's notes didn't mention anything about this change.

Components/BrowseFunc.pas
@@ -15,7 +15,7 @@ uses
   Windows, Classes, Forms;
 
 function BrowseForFolder(const Prompt: String; var Directory: String;
-  const ParentWnd: HWND; const NewFolderButton: Boolean): Boolean;
+  const ParentWnd: HWND): Boolean;
 function NewGetOpenFileName(const Prompt: String; var FileName: String;
   const InitialDirectory, Filter, DefaultExtension: String;
   const ParentWnd: HWND): Boolean;
@@ -34,7 +34,7 @@ uses
   PathFunc;
 
 function BrowseForFolder(const Prompt: String; var Directory: String;
-  const ParentWnd: HWND; const NewFolderButton: Boolean): Boolean;
+  const ParentWnd: HWND): Boolean;
 
   function RemoveSinglePeriod(const S: String): String;
   begin

ISHelp/isxfunc.xml
@@ -3285,8 +3285,9 @@ end;</pre></example>
       </function>
       <function>
         <name>BrowseForFolder</name>
-        <prototype>function BrowseForFolder(const Prompt: String; var Directory: String; const NewFolderButton: Boolean): Boolean;</prototype>
-        <description><p>Displays a dialog box that enables the user to select a directory. The current value of Directory is used as the initially selected directory. If NewFolderButton is True, a <i>New Folder</i> button will be shown, allowing the user to create new folders. Returns True if the user selected a directory and clicked OK, False otherwise. The selected directory is returned in the Directory string.</p></description>
+        <prototype>function BrowseForFolder(const Prompt: String; var Directory: String; const Reserved: Boolean): Boolean;</prototype>
+        <description><p>Displays a dialog box that enables the user to select a directory. The current value of Directory is used as the initially selected directory. Returns True if the user selected a directory and clicked OK, False otherwise. The selected directory is returned in the Directory string.</p>
+<p>Parameter Reserved is ignored, but must be specified anyway.</p></description>
       </function>
       <function>
         <name>ExitSetupMsgBox</name>

Projects/Src/IDE.Wizard.WizardForm.pas
@@ -878,7 +878,7 @@ begin
   Path := OutputDirEdit.Text;
   if PathDrivePartLength(Path) = 0 then
     Path := '';  { don't pass in a relative path to BrowseForFolder }
-  if BrowseForFolder(LFmtMessage(SWizardCompilerOutputDir), Path, Handle, True) then
+  if BrowseForFolder(LFmtMessage(SWizardCompilerOutputDir), Path, Handle) then
     OutputDirEdit.Text := Path;
 end;
 

Projects/Src/IDE.Wizard.WizardFormFilesHelper.pas
@@ -183,7 +183,7 @@ var
   Recurse: Boolean;
 begin
   Path := '';
-  if BrowseForFolder(LFmtMessage(SWizardAppFiles3), Path, FForm.Handle, False) then begin
+  if BrowseForFolder(LFmtMessage(SWizardAppFiles3), Path, FForm.Handle) then begin
     case MsgBox(LFmtMessage(SWizardAppFilesSubDirsMessage, [Path]), '', mbConfirmation, MB_YESNOCANCEL) of
       IDYES: Recurse := True;
       IDNO: Recurse := False;

Projects/Src/Setup.NewDiskForm.pas
@@ -2,7 +2,7 @@ unit Setup.NewDiskForm;
 
 {
   Inno Setup
-  Copyright (C) 1997-2025 Jordan Russell
+  Copyright (C) 1997-2026 Jordan Russell
   Portions by Martijn Laan
   For conditions of distribution and use, see LICENSE.TXT.
 
@@ -114,7 +114,7 @@ var
   Dir: String;
 begin
   Dir := GetSanitizedPath;
-  if BrowseForFolder(SetupMessages[msgSelectDirectoryLabel], Dir, Handle, False) then
+  if BrowseForFolder(SetupMessages[msgSelectDirectoryLabel], Dir, Handle) then
     PathEdit.Text := Dir;
 end;
 

Projects/Src/Setup.ScriptFunc.pas
@@ -322,7 +322,7 @@ var
     RegisterScriptFunc('BROWSEFORFOLDER', procedure(const Caller: TPSExec; const OrgName: AnsiString; const Stack: TPSStack; const PStart: Integer)
     begin
       var S := Stack.GetString(PStart-2);
-      Stack.SetBool(PStart, BrowseForFolder(Stack.GetString(PStart-1), S, GetOwnerWndForMessageBox, Stack.GetBool(PStart-3)));
+      Stack.SetBool(PStart, BrowseForFolder(Stack.GetString(PStart-1), S, GetOwnerWndForMessageBox));
       Stack.SetString(PStart-2, S);
     end);
     RegisterScriptFunc('GETOPENFILENAME', procedure(const Caller: TPSExec; const OrgName: AnsiString; const Stack: TPSStack; const PStart: Integer)

Projects/Src/Shared.ScriptFunc.pas
@@ -334,7 +334,7 @@ initialization
 
   ScriptFuncTables[sftBrowseFunc] :=
   [
-    'function BrowseForFolder(const Prompt: String; var Directory: String; const NewFolderButton: Boolean): Boolean;',
+    'function BrowseForFolder(const Prompt: String; var Directory: String; const Reserved: Boolean): Boolean;',
     'function GetOpenFileName(const Prompt: String; var FileName: String; const InitialDirectory, Filter, DefaultExtension: String): Boolean;',
     'function GetOpenFileNameMulti(const Prompt: String; const FileNameList: TStrings; const InitialDirectory, Filter, DefaultExtension: String): Boolean;',
     'function GetSaveFileName(const Prompt: String; var FileName: String; const InitialDirectory, Filter, DefaultExtension: String): Boolean;'