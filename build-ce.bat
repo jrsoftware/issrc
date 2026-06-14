jrsoftware/issrc

commit bcfd2fcfbd497659159060918354045c89380b55

Update build.bat to build both versions. Removes a pause as well. Also removed the option to do 'build setup' to skip the compiles.
Build-ce.bat still only builds x64, but did update it to keep it consistent with build.bat.

Build.yml's Files artifact is now named Files-x64 because it has x64 files only.

build-ce.bat
@@ -13,7 +13,7 @@ rem
 rem  This batch files does the following things:
 rem  -Ask the user to compile Inno Setup including ISSigTool and ISHelpGen after clearing output first
 rem  -Compile ISetup*.chm
-rem  -Create Inno Setup installer
+rem  -Create 64-bit Inno Setup installer
 rem
 rem  Once done the installer can be found in Output
 
@@ -55,6 +55,24 @@ rem  Embed user's public key into sources
 call .\issig.bat embed
 if errorlevel 1 goto failed
 echo ISSigTool embed done
+
+if not exist files\ishelpgen.exe (
+  echo Missing ISHelpGen
+  echo Now open Projects\Projects.groupproj and build the ISHelpGen project and its Win64 target in Release mode
+
+  echo - Waiting for file...
+  call :waitforfile files\ishelpgen.exe
+  echo Compiling ISHelpGen done
+)
+
+cd ishelp
+if errorlevel 1 goto failed
+call .\compile.bat
+if errorlevel 1 goto failed
+cd ..
+if errorlevel 1 goto failed
+echo Compiling ISetup*.chm done
+pause
 
 echo.
 call :deletefile files\iside.exe
@@ -103,16 +121,6 @@ rem  Sign using user's private key - also see compile.bat
 call .\issig.bat sign Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\Setup.e64 Files\SetupCustomStyle.e32 Files\SetupCustomStyle.e64 Files\SetupLdr.e32 Files\SetupLdr.e64
 if errorlevel 1 goto failed
 echo ISSigTool sign done
-pause
-
-cd ishelp
-if errorlevel 1 goto failed
-call .\compile.bat
-if errorlevel 1 goto failed
-cd ..
-if errorlevel 1 goto failed
-echo Compiling ISetup*.chm done
-pause
 
 :setup
 echo - Setup.exe

.github/workflows/build.yml
@@ -56,7 +56,7 @@ jobs:
       - name: Upload Files/
         uses: actions/upload-artifact@v4
         with:
-          name: Files
+          name: Files-x64
           path: Files/
       - name: Upload Output/
         uses: actions/upload-artifact@v4
@@ -84,13 +84,13 @@ jobs:
         run: |
           set -x &&
           ver="$(sed -n 's/^set VER=//p' <build.bat)" &&
-          mt '-inputresource:Output\innosetup-'$ver.exe -out:innosetup-$ver.manifest &&
-          cat innosetup-$ver.manifest &&
-          mkdir -p Output/innosetup-$ver.exe.Local &&
-          cp -R "$(cygpath -au "$SYSTEMROOT")"/WinSxS/x86_microsoft.windows.common-controls_* Output/innosetup-$ver.exe.Local/ &&
-          mkdir Output/innosetup-$ver.en-US &&
-          mkdir Output/innosetup-$ver.en &&
-          mkdir Output/innosetup-$ver.ENU &&
+          mt '-inputresource:Output\innosetup-'$ver-x86.exe -out:innosetup-$ver-x86.manifest &&
+          cat innosetup-$ver-x86.manifest &&
+          mkdir -p Output/innosetup-$ver-x86.exe.Local &&
+          cp -R "$(cygpath -au "$SYSTEMROOT")"/WinSxS/x86_microsoft.windows.common-controls_* Output/innosetup-$ver-x86.exe.Local/ &&
+          mkdir Output/innosetup-$ver-x86.en-US &&
+          mkdir Output/innosetup-$ver-x86.en &&
+          mkdir Output/innosetup-$ver-x86.ENU &&
           mkdir -p trace &&
           echo "$ver" >trace/ver &&
           curl -LO https://download.sysinternals.com/files/ProcessMonitor.zip &&
@@ -101,7 +101,7 @@ jobs:
           test $? = 0 &&
           ps -W &&
           ./Procmon.exe -AcceptEula -WaitForIdle &&
-          ./Output/innosetup-$ver.exe //verysilent //suppressmsgboxes //dir=InnoSetup //noicons \
+          ./Output/innosetup-$ver-x86.exe //verysilent //suppressmsgboxes //dir=InnoSetup //noicons \
             //tasks= //portable=1 //currentuser &&
           test -x InnoSetup/ISCC.exe &&
           ./Procmon.exe -Terminate -Quiet &&
@@ -119,7 +119,7 @@ jobs:
           set -x &&
           curdir="$(cygpath -aw Output | sed 's/\\/&&/g')" &&
           ver="$(sed 's/\./\\&/g' <trace/ver)" &&
-          sed -ne '/"'$curdir'\\innosetup-'$ver'\.\(exe\|exe\.Config\|en-US\|en\|ENU\|EN\)"/d' \
+          sed -ne '/"'$curdir'\\innosetup-'$ver'-x86\.\(exe\|exe\.Config\|en-US\|en\|ENU\|EN\)"/d' \
             -e '/"System","[0-9]*","\(CreateFileMapping\|FASTIO_[A-Z_]*\)"/d' -e '/"'$curdir'\\/p' \
             trace/procmon.csv >trace/filtered.csv &&
           if test -s trace/filtered.csv

build.bat
@@ -14,7 +14,7 @@ rem  This batch files does the following things:
 rem  -Compile ISHelpGen
 rem  -Compile ISetup*.chm
 rem  -Compile Inno Setup including ISSigTool
-rem  -Create Inno Setup installer
+rem  -Create 32-bit and 64-bit Inno Setup installers
 rem
 rem  Once done the installer can be found in Output
 
@@ -26,9 +26,6 @@ echo Building Inno Setup %VER%...
 echo.
 
 cd /d %~dp0
-
-if /I "%1"=="setup" goto setup
-if not "%1"=="" goto failed
 
 call .\compile.bat x64 issigtool
 if errorlevel 1 goto failed
@@ -52,9 +49,42 @@ call .\issig.bat embed
 if errorlevel 1 goto failed
 echo ISSigTool embed done
 
+call .\compile.bat x64 ishelpgen
+if errorlevel 1 goto failed
+echo Compiling ISHelpGen done
+
+cd ishelp
+if errorlevel 1 goto failed
 call .\compile.bat
 if errorlevel 1 goto failed
-echo Compiling Inno Setup done
+cd ..
+if errorlevel 1 goto failed
+echo Compiling ISetup*.chm done
+pause
+
+call :build x86
+
+echo Cleaning output of previous build
+del Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\Setup.e64 Files\SetupCustomStyle.e32 Files\SetupCustomStyle.e64 Files\SetupLdr.e32 Files\SetupLdr.e64
+if errorlevel 1 goto failed
+del Files\ISIDE.exe Files\ISCC.exe Files\ISSigTool.exe
+if errorlevel 1 goto failed
+
+call :build x64
+
+echo All done!
+pause
+exit /b 0
+
+:failed
+echo *** FAILED ***
+pause
+exit /b 1
+
+:build
+call .\compile.bat %~1
+if errorlevel 1 goto failed
+echo Compiling %~1 Inno Setup done
 
 if exist .\setup-presign.bat (
   echo - Presigning
@@ -66,44 +96,28 @@ if exist .\setup-presign.bat (
 rem  Sign using user's private key - also see compile.bat
 call .\issig.bat sign Files\ISCmplr.dll Files\ISPP.dll Files\Setup.e32 Files\Setup.e64 Files\SetupCustomStyle.e32 Files\SetupCustomStyle.e64 Files\SetupLdr.e32 Files\SetupLdr.e64
 if errorlevel 1 goto failed
-echo ISSigTool sign done
-pause
-
-cd ishelp
-if errorlevel 1 goto failed
-call .\compile.bat
-if errorlevel 1 goto failed
-cd ..
-if errorlevel 1 goto failed
-echo Compiling ISetup*.chm done
-pause
+echo ISSigTool sign %~1 done
 
 :setup
-echo - Setup.exe
+echo - %~1 Setup.exe
 if exist .\setup-sign.bat (
-  call .\setup-sign.bat
+  call .\setup-sign.bat /D%~1
 ) else (
-  files\iscc setup.iss
+  files\iscc setup.iss /D%~1
 )
 if errorlevel 1 goto failed
-echo - Renaming files
+echo - Renaming %~1 files
 cd output
 if errorlevel 1 goto failed
-move /y mysetup.exe innosetup-%VER%.exe
+move /y mysetup.exe innosetup-%VER%-%~1.exe
 if errorlevel 1 goto failed
 cd ..
 if errorlevel 1 goto failed
-echo Creating Inno Setup installer done
-call .\issig.bat sign output\innosetup-%VER%.exe
+echo Creating %~1 Inno Setup installer done
+call .\issig.bat sign output\innosetup-%VER%-%~1.exe
 if errorlevel 1 goto failed
-powershell.exe -NoProfile -Command "Write-Host -NoNewline 'SHA256 hash: '; (Get-FileHash -Algorithm SHA256 -Path output\innosetup-%VER%.exe).Hash.ToLower()"
+
+powershell.exe -NoProfile -Command "Write-Host -NoNewline 'SHA256 hash: '; (Get-FileHash -Algorithm SHA256 -Path output\innosetup-%VER%-%~1.exe).Hash.ToLower()"
 rem ignoring error here
 
-echo All done!
-pause
-exit /b 0
-
-:failed
-echo *** FAILED ***
-pause
-exit /b 1
+exit /b

commit 86ab38cdeac4013abf8e5d543a95619c83fd7ffb

build-ce.bat: Fix issue introduced by bcfd2fcf.

build-ce.bat
@@ -56,12 +56,12 @@ call .\issig.bat embed
 if errorlevel 1 goto failed
 echo ISSigTool embed done
 
-if not exist files\ishelpgen.exe (
+if not exist ishelp\ishelpgen\ishelpgen.exe (
   echo Missing ISHelpGen
   echo Now open Projects\Projects.groupproj and build the ISHelpGen project and its Win64 target in Release mode
 
   echo - Waiting for file...
-  call :waitforfile files\ishelpgen.exe
+  call :waitforfile ishelp\ishelpgen\ishelpgen.exe
   echo Compiling ISHelpGen done
 )
 