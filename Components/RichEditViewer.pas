b3b29fcabfbdf5c28b4ac239745b093b17ef3b3f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-06-24 14:36:37 +0200

    IDE.RichEdit: Move the RichEdit OLE callback interface and basic implementation to a separate unit.

--- Components/RichEditViewer.pas
+++ Components/RichEditViewer.pas
@@ -11,33 +11,12 @@ unit RichEditViewer;
 interface
 
 uses
-  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  Windows, Messages, RichEdit, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
   {$IFDEF VCLSTYLES} Vcl.Themes, {$ELSE} Themes, {$ENDIF}
-  StdCtrls, RichEdit, ActiveX;
+  StdCtrls,
+  RichEditOleCallback;
 
 type
-  IRichEditOleCallback = interface(IUnknown)
-    ['{00020d03-0000-0000-c000-000000000046}']
-    function GetNewStorage(out stg: IStorage): HResult; stdcall;
-    function GetInPlaceContext(out Frame: IOleInPlaceFrame;
-      out Doc: IOleInPlaceUIWindow;
-      lpFrameInfo: POleInPlaceFrameInfo): HResult; stdcall;
-    function ShowContainerUI(fShow: BOOL): HResult; stdcall;
-    function QueryInsertObject(const clsid: TCLSID; const stg: IStorage;
-      cp: Integer): HResult; stdcall;
-    function DeleteObject(const oleobj: IOleObject): HResult; stdcall;
-    function QueryAcceptData(const dataobj: IDataObject;
-      var cfFormat: TClipFormat; reco: DWORD; fReally: BOOL;
-      hMetaPict: HGLOBAL): HResult; stdcall;
-    function ContextSensitiveHelp(fEnterMode: BOOL): HResult; stdcall;
-    function GetClipboardData(const chrg: TCharRange; reco: DWORD;
-      out dataobj: IDataObject): HResult; stdcall;
-    function GetDragDropEffect(fDrag: BOOL; grfKeyState: DWORD;
-      var dwEffect: DWORD): HResult; stdcall;
-    function GetContextMenu(seltype: Word; const oleobj: IOleObject;
-      const chrg: TCharRange; out menu: HMENU): HResult; stdcall;
-  end;
-
   TRichEditViewerCustomShellExecute = procedure(hWnd: HWND; Operation, FileName, Parameters, Directory: LPWSTR; ShowCmd: Integer); stdcall;
 
   TRichEditViewer = class(TMemo)
@@ -82,7 +61,7 @@ procedure Register;
 implementation
 
 uses
-  ShellApi, PathFunc, ComObj;
+  ShellApi, PathFunc;
 
 {$IF RtlVersion < 36.0}
 const
@@ -90,29 +69,6 @@ const
 {$ENDIF}
 
 type
- { Basic implementation of IRichEditOleCallback to enable the viewing of images and other objects. }
-  TBasicRichEditOleCallback = class(TInterfacedObject, IRichEditOleCallback)
-  public
-    function GetNewStorage(out stg: IStorage): HResult; stdcall;
-    function GetInPlaceContext(out Frame: IOleInPlaceFrame;
-      out Doc: IOleInPlaceUIWindow;
-      lpFrameInfo: POleInPlaceFrameInfo): HResult; stdcall;
-    function ShowContainerUI(fShow: BOOL): HResult; stdcall;
-    function QueryInsertObject(const clsid: TCLSID; const stg: IStorage;
-      cp: Integer): HResult; stdcall;
-    function DeleteObject(const oleobj: IOleObject): HResult; stdcall;
-    function QueryAcceptData(const dataobj: IDataObject;
-      var cfFormat: TClipFormat; reco: DWORD; fReally: BOOL;
-      hMetaPict: HGLOBAL): HResult; stdcall;
-    function ContextSensitiveHelp(fEnterMode: BOOL): HResult; stdcall;
-    function GetClipboardData(const chrg: TCharRange; reco: DWORD;
-      out dataobj: IDataObject): HResult; stdcall;
-    function GetDragDropEffect(fDrag: BOOL; grfKeyState: DWORD;
-      var dwEffect: DWORD): HResult; stdcall;
-    function GetContextMenu(seltype: Word; const oleobj: IOleObject;
-      const chrg: TCharRange; out menu: HMENU): HResult; stdcall;
-  end;
-
 {$IF RtlVersion < 36.0}
   PEnLink = ^TEnLink;
   TENLink = record
@@ -260,76 +216,6 @@ begin
       RichEditModule := 0;
     end;
   end;
-end;
-
-{ TBasicRichEditOleCallback }
-
-function TBasicRichEditOleCallback.GetNewStorage(out stg: IStorage): HResult; stdcall;
-var
-  LockBytes: ILockBytes;
-begin
-  try
-    OleCheck(CreateILockBytesOnHGlobal(0, True, LockBytes));
-    OleCheck(StgCreateDocfileOnILockBytes(LockBytes, STGM_READWRITE
-      or STGM_SHARE_EXCLUSIVE or STGM_CREATE, 0, stg));
-    Result := S_OK;
-  except
-    Result := E_OUTOFMEMORY;
-  end;
-end;
-
-function TBasicRichEditOleCallback.GetInPlaceContext(out Frame: IOleInPlaceFrame;
-  out Doc: IOleInPlaceUIWindow; lpFrameInfo: POleInPlaceFrameInfo): HResult;
-begin
-  Result := E_NOTIMPL;
-end;
-
-function TBasicRichEditOleCallback.ShowContainerUI(fShow: BOOL): HResult;
-begin
-  Result := E_NOTIMPL;
-end;
-
-function TBasicRichEditOleCallback.QueryInsertObject(const clsid: TCLSID; const stg: IStorage;
-  cp: Integer): HResult;
-begin
-  Result := S_OK;
-end;
-
-function TBasicRichEditOleCallback.DeleteObject(const oleobj: IOleObject): HResult;
-begin
-  if Assigned(oleobj) then
-    oleobj.Close(OLECLOSE_NOSAVE);
-  Result := S_OK;
-end;
-
-function TBasicRichEditOleCallback.QueryAcceptData(const dataobj: IDataObject;
-  var cfFormat: TClipFormat; reco: DWORD; fReally: BOOL;
-  hMetaPict: HGLOBAL): HResult;
-begin
-  Result := S_OK;
-end;
-
-function TBasicRichEditOleCallback.ContextSensitiveHelp(fEnterMode: BOOL): HResult;
-begin
-  Result := S_OK;
-end;
-
-function TBasicRichEditOleCallback.GetClipboardData(const chrg: TCharRange; reco: DWORD;
-  out dataobj: IDataObject): HResult;
-begin
-  Result := E_NOTIMPL;
-end;
-
-function TBasicRichEditOleCallback.GetDragDropEffect(fDrag: BOOL; grfKeyState: DWORD;
-  var dwEffect: DWORD): HResult;
-begin
-  Result := E_NOTIMPL;
-end;
-
-function TBasicRichEditOleCallback.GetContextMenu(seltype: Word;
-  const oleobj: IOleObject; const chrg: TCharRange; out Menu: HMENU): HResult;
-begin
-  Result := E_NOTIMPL;
 end;
 
 { TRichEditViewer }
