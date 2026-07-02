9386a3cb8f23064512e7f890dc3f64e899ee7948
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-07-02 16:50:16 +0200

    test.bat: Allow single arch test.

--- test.bat
+++ test.bat
@@ -11,14 +11,21 @@ setlocal
 
 cd /d %~dp0
 
-call :test x86
-if errorlevel 1 goto failed
+if "%1"=="" (
+  set "Archs=x86 x64"
+) else if "%1"=="x86" (
+  set "Archs=x86"
+) else if "%1"=="x64" (
+  set "Archs=x64"
+) else (
+  echo Architecture parameter is invalid. Must be omitted or be "x86" or "x64".
+  goto failed
+)
 
-echo Cleaning output of previous build
-del Files\ISTestTool.exe
-
-call :test x64
-if errorlevel 1 goto failed
+for %%a in (%Archs%) do (
+  call :test %%a
+  if errorlevel 1 goto failed
+)
 
 echo Success!
 
@@ -29,6 +36,9 @@ echo *** FAILED ***
 exit /b 1
 
 :test
+echo Cleaning output of previous build
+if exist Files\ISTestTool.exe del Files\ISTestTool.exe
+
 call .\compile.bat %~1 ISTestTool
 if errorlevel 1 exit /b 1
 echo Compiling %~1 Inno Setup Test Tool done
