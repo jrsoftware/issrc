7fc483339b6f5b531eb4c0c504a8026877c9142f
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-07-19 16:23:43 +0200

    IDE: Fix missing F1 fake shortcut.

--- Projects/Src/IDE.MainForm.pas
+++ Projects/Src/IDE.MainForm.pas
@@ -1053,6 +1053,8 @@ begin
   SetFakeShortCut(POutputListSelectAll, Ord('A'), [ssCtrl]);
   { Set real shortcut on TOptions which can't be set at design time }
   TOptions.ShortCut := ShortCut(VK_OEM_COMMA, [ssCtrl]);
+  { F1 is handled by the memo's and by the inspector }
+  SetFakeShortCut(HDoc, VK_F1, []);
 
   PopupMenu := TMainFormPopupMenu.Create(Self, EMenu);
 
