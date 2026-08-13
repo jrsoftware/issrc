8f057c89803221dac79c169664d4aa0648ce07af
Martijn Laan <1092369+martijnlaan@users.noreply.github.com>
2026-08-13 11:32:07 +0200

    TScintEdit.UpdateStyleAttributes: Fix the SCI_STYLESETFONT call: it expects UTF-8.
    From Scintilla docs: "The fontName is a zero terminated string holding the name of a font. Under Windows ... the name is decoded as UTF-8"
    
    Also unrelated formatting cleanup.

--- Components/ScintEdit.pas
+++ Components/ScintEdit.pas
@@ -2572,10 +2572,11 @@ var
     const Attr: TScintStyleAttributes; const Force: Boolean);
   begin
     if Force or (Attr.FontName <> DefaultAttr.FontName) then
-      Call(SCI_STYLESETFONT, StyleNumber, AnsiString(Attr.FontName));
-    if Force or (Attr.FontSize <> DefaultAttr.FontSize) then
+      Call(SCI_STYLESETFONT, StyleNumber, UTF8String(Attr.FontName));
+    if Force or (Attr.FontSize <> DefaultAttr.FontSize) then begin
       { Note: Scintilla doesn't support negative point sizes like the VCL }
       Call(SCI_STYLESETSIZE, StyleNumber, Abs(Attr.FontSize));
+    end;
     if Force or (Attr.FontCharset <> DefaultAttr.FontCharset) then
       Call(SCI_STYLESETCHARACTERSET, StyleNumber, Attr.FontCharset);
     if Force or (Attr.FontStyle <> DefaultAttr.FontStyle) then begin
