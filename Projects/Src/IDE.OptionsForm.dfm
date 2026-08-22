jrsoftware/issrc

commit e006b99ed0260186a9f966f7667cb55dcf5134d2

IDE.Inspector: Add FollowCaret option UI.

Projects/Src/IDE.OptionsForm.dfm
@@ -4,7 +4,7 @@ object OptionsForm: TOptionsForm
   BorderIcons = [biSystemMenu]
   BorderStyle = bsDialog
   Caption = 'Options'
-  ClientHeight = 508
+  ClientHeight = 528
   ClientWidth = 669
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
@@ -17,7 +17,7 @@ object OptionsForm: TOptionsForm
   OnShow = FormShow
   DesignSize = (
     669
-    508)
+    528)
   TextHeight = 13
   object GroupBox1: TNewGroupBox
     Left = 8
@@ -193,7 +193,7 @@ object OptionsForm: TOptionsForm
     Left = 8
     Top = 376
     Width = 321
-    Height = 88
+    Height = 108
     Caption = ' Inspector '
     TabOrder = 1
     object InspectorShowAllKnownDirectivesCheck: TCheckBox
@@ -205,30 +205,39 @@ object OptionsForm: TOptionsForm
       Caption = 'Show all known directives'
       TabOrder = 0
     end
-    object InspectorQuoteNewDirectiveValuesCheck: TCheckBox
+    object InspectorFollowCaretCheck: TCheckBox
       Left = 8
       Top = 40
       Width = 305
       Height = 17
       Anchors = [akLeft, akTop, akRight]
-      Caption = 'Quote new directive values'
+      Caption = 'Follow cursor'
       TabOrder = 1
     end
-    object InspectorQuoteNewParameterValuesCheck: TCheckBox
+    object InspectorQuoteNewDirectiveValuesCheck: TCheckBox
       Left = 8
       Top = 60
       Width = 305
       Height = 17
       Anchors = [akLeft, akTop, akRight]
-      Caption = 'Quote new parameter values'
+      Caption = 'Quote new directive values'
       TabOrder = 2
+    end
+    object InspectorQuoteNewParameterValuesCheck: TCheckBox
+      Left = 8
+      Top = 80
+      Width = 305
+      Height = 17
+      Anchors = [akLeft, akTop, akRight]
+      Caption = 'Quote new parameter values'
+      TabOrder = 3
     end
   end
   object GroupBox3: TNewGroupBox
     Left = 340
     Top = 8
     Width = 321
-    Height = 405
+    Height = 425
     Anchors = [akLeft, akTop, akBottom]
     Caption = ' Editor '
     TabOrder = 2
@@ -306,7 +315,7 @@ object OptionsForm: TOptionsForm
     end
     object Label1: TNewStaticText
       Left = 8
-      Top = 339
+      Top = 359
       Width = 25
       Height = 14
       Anchors = [akLeft, akBottom]
@@ -315,7 +324,7 @@ object OptionsForm: TOptionsForm
     end
     object FontPanel: TPanel
       Left = 104
-      Top = 330
+      Top = 350
       Width = 129
       Height = 32
       Anchors = [akLeft, akRight, akBottom]
@@ -326,7 +335,7 @@ object OptionsForm: TOptionsForm
     end
     object ChangeFontButton: TButton
       Left = 240
-      Top = 335
+      Top = 355
       Width = 73
       Height = 23
       Anchors = [akRight, akBottom]
@@ -336,7 +345,7 @@ object OptionsForm: TOptionsForm
     end
     object Label2: TNewStaticText
       Left = 8
-      Top = 373
+      Top = 393
       Width = 54
       Height = 14
       Anchors = [akLeft, akBottom]
@@ -346,7 +355,7 @@ object OptionsForm: TOptionsForm
     end
     object TabWidthEdit: TEdit
       Left = 104
-      Top = 370
+      Top = 390
       Width = 50
       Height = 21
       Anchors = [akLeft, akRight, akBottom]
@@ -364,7 +373,7 @@ object OptionsForm: TOptionsForm
     end
     object Label4: TNewStaticText
       Left = 8
-      Top = 305
+      Top = 325
       Width = 37
       Height = 14
       Anchors = [akLeft, akBottom]
@@ -374,7 +383,7 @@ object OptionsForm: TOptionsForm
     end
     object ThemeComboBox: TComboBox
       Left = 104
-      Top = 301
+      Top = 321
       Width = 209
       Height = 21
       Style = csDropDownList
@@ -401,7 +410,7 @@ object OptionsForm: TOptionsForm
     end
     object Label5: TNewStaticText
       Left = 8
-      Top = 276
+      Top = 296
       Width = 27
       Height = 14
       Anchors = [akLeft, akBottom]
@@ -411,7 +420,7 @@ object OptionsForm: TOptionsForm
     end
     object MemoKeyMappingComboBox: TComboBox
       Left = 104
-      Top = 272
+      Top = 292
       Width = 209
       Height = 21
       Style = csDropDownList
@@ -420,7 +429,7 @@ object OptionsForm: TOptionsForm
     end
     object ShowWhiteSpaceCheck: TCheckBox
       Left = 160
-      Top = 372
+      Top = 392
       Width = 153
       Height = 17
       Anchors = [akRight, akBottom]
@@ -439,7 +448,7 @@ object OptionsForm: TOptionsForm
   end
   object OKButton: TButton
     Left = 500
-    Top = 476
+    Top = 496
     Width = 73
     Height = 23
     Anchors = [akRight, akBottom]
@@ -450,7 +459,7 @@ object OptionsForm: TOptionsForm
   end
   object CancelButton: TButton
     Left = 580
-    Top = 476
+    Top = 496
     Width = 73
     Height = 23
     Anchors = [akRight, akBottom]
@@ -461,7 +470,7 @@ object OptionsForm: TOptionsForm
   end
   object AssocButton: TButton
     Left = 16
-    Top = 476
+    Top = 496
     Width = 201
     Height = 23
     Anchors = [akLeft, akBottom]

Projects/Src/IDE.Inspector.pas
@@ -121,7 +121,7 @@ type
     constructor Create(const AJvInspector: TJvInspector;
       const ANoteText: TNewStaticText;
       const AFactory: TLiveScriptObjectFactory;
-      const AShowAllKnownDirectives: Boolean;
+      const AShowAllKnownDirectives, AFollowCaret: Boolean;
       const AOnGetBaseDir: TInspectorGetBaseDirEvent);
     destructor Destroy; override;
     procedure ForceFinishEdit(const AForceCancel: Boolean = False);
@@ -200,7 +200,7 @@ end;
 constructor TInspector.Create(const AJvInspector: TJvInspector;
   const ANoteText: TNewStaticText;
   const AFactory: TLiveScriptObjectFactory;
-  const AShowAllKnownDirectives: Boolean;
+  const AShowAllKnownDirectives, AFollowCaret: Boolean;
   const AOnGetBaseDir: TInspectorGetBaseDirEvent);
 { Takes ownership of AJvInspector but not of ANoteText }
 begin
@@ -210,6 +210,7 @@ begin
   FFactory := AFactory;
   FOnGetBaseDir := AOnGetBaseDir;
   FShowAllKnownDirectives := AShowAllKnownDirectives;
+  FFollowCaret := AFollowCaret;
   {$IFDEF DEBUG}
   FDebugStatusRowString := 'Not updated yet';
   {$ENDIF}

Projects/Src/IDE.MainForm.UpdateMenuHelper.pas
@@ -534,7 +534,7 @@ begin
   PInspectorShowAllKnownDirectives.Visible := FInspector.ShowingDirectiveSection;
   PInspectorShowAllKnownDirectives.Checked := FOptions.InspectorShowAllKnownDirectives;
   PInspectorShowAllKnownDirectives.Enabled := FActiveMemo = FMainMemo;
-  PInspectorShowAllKnownDirectivesSeparator.Visible := PInspectorShowAllKnownDirectives.Visible; { AutoLineReduction is maManual }
+  PInspectorFollowCaret.Checked := FOptions.InspectorFollowCaret;
 
   _ApplyMenuBitmapsAndNewShortCutText(Menu);
 end;

Projects/Src/IDE.MainForm.dfm
@@ -840,7 +840,11 @@ object MainForm: TMainForm
           Caption = 'Show all known directives'
           OnClick = PInspectorShowAllKnownDirectivesClick
         end
-        object PInspectorShowAllKnownDirectivesSeparator: TMenuItem
+        object PInspectorFollowCaret: TMenuItem
+          Caption = 'Follow cursor'
+          OnClick = PInspectorFollowCaretClick
+        end
+        object N27: TMenuItem
           Caption = '-'
         end
         object PInspectorHelp: TMenuItem

Projects/Src/IDE.MainForm.pas
@@ -147,6 +147,7 @@ type
     AutoHideNewIncludedFiles: Boolean;
     ShowCaretPosition: Boolean;
     InspectorShowAllKnownDirectives: Boolean;
+    InspectorFollowCaret: Boolean;
     InspectorQuoteNewParameterValues: Boolean;
     InspectorQuoteNewDirectiveValues: Boolean;
   end;
@@ -349,7 +350,8 @@ type
     PInspectorRemove: TMenuItem;
     N26: TMenuItem;
     PInspectorShowAllKnownDirectives: TMenuItem;
-    PInspectorShowAllKnownDirectivesSeparator: TMenuItem;
+    PInspectorFollowCaret: TMenuItem;
+    N27: TMenuItem;
     procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
     procedure FExitClick(Sender: TObject);
     procedure FOpenMainFileClick(Sender: TObject);
@@ -502,6 +504,7 @@ type
     procedure PInspectorHelpClick(Sender: TObject);
     procedure PInspectorRemoveClick(Sender: TObject);
     procedure PInspectorShowAllKnownDirectivesClick(Sender: TObject);
+    procedure PInspectorFollowCaretClick(Sender: TObject);
   private
     FCompilerVersion: PCompilerVersionInfo;
     FOptionsLoaded: Boolean;
@@ -928,11 +931,13 @@ constructor TMainForm.Create(AOwner: TComponent);
     const Ini = TConfigIniFile.Create;
     try
       FOptions.InspectorShowAllKnownDirectives := Ini.ReadBool('Options', 'InspectorShowAllKnownDirectives', True);
+      FOptions.InspectorFollowCaret := Ini.ReadBool('Options', 'InspectorFollowCaret', True);
     finally
       Ini.Free;
     end;
     FInspector := TInspector.Create(JvInspector, InspectorNoteText, LiveScriptObjectFactoryForMemo(FActiveMemo),
-      FOptions.InspectorShowAllKnownDirectives, GetMainBaseDir); { No main-memo check needed: FActiveMemo is FMainMemo at startup }
+      FOptions.InspectorShowAllKnownDirectives, FOptions.InspectorFollowCaret,
+      GetMainBaseDir); { No main-memo check needed: FActiveMemo is FMainMemo at startup }
   end;
 
   procedure ReadAndApplyConfig;
@@ -987,7 +992,7 @@ constructor TMainForm.Create(AOwner: TComponent);
       I := Ini.ReadInteger('Options', 'MemoKeyMappingType', Ord(GetDefaultMemoKeyMappingType));
       if (I >= 0) and (I <= Ord(High(TIDEScintKeyMappingType))) then
         FOptions.MemoKeyMappingType := TIDEScintKeyMappingType(I);
-      { FOptions.InspectorShowAllKnownDirectives already loaded }
+      { FOptions.InspectorShowAllKnownDirectives and InspectorFollowCaret already loaded }
       FOptions.InspectorQuoteNewParameterValues := Ini.ReadBool('Options', 'InspectorQuoteNewParameterValues', True);
       FOptions.InspectorQuoteNewDirectiveValues := Ini.ReadBool('Options', 'InspectorQuoteNewDirectiveValues', False);
       FMainMemo.Font.Name := Ini.ReadString('Options', 'EditorFontName', FMainMemo.Font.Name);
@@ -2700,6 +2705,7 @@ begin
   const ActiveMemoIsMainMemo = (FActiveMemo = FMainMemo);
   FInspector.ShowAllKnownDirectives := FOptions.InspectorShowAllKnownDirectives and ActiveMemoIsMainMemo;
   FInspector.ShowAllKnownDirectivesSuppressedNote := FOptions.InspectorShowAllKnownDirectives and not ActiveMemoIsMainMemo;
+  FInspector.FollowCaret := FOptions.InspectorFollowCaret;
   FInspector.QuoteNewDirectiveValues := FOptions.InspectorQuoteNewDirectiveValues;
   FInspector.QuoteNewParameterValues := FOptions.InspectorQuoteNewParameterValues;
 end;
@@ -3957,6 +3963,18 @@ begin
   end;
 end;
 
+procedure TMainForm.PInspectorFollowCaretClick(Sender: TObject);
+begin
+  FOptions.InspectorFollowCaret := not FOptions.InspectorFollowCaret;
+  SyncInspectorOptions;
+  const Ini = TConfigIniFile.Create;
+  try
+    Ini.WriteBool('Options', 'InspectorFollowCaret', FOptions.InspectorFollowCaret);
+  finally
+    Ini.Free;
+  end;
+end;
+
 procedure TMainForm.UpdateOccurrenceIndicators(const AMemo: TIDEScintEdit);
 
   procedure FindTextAndAddRanges(const AMemo: TIDEScintEdit;
@@ -4350,6 +4368,7 @@ begin
     OptionsForm.HighlightWordAtCursorOccurrencesCheck.Checked := FOptions.HighlightWordAtCursorOccurrences;
     OptionsForm.HighlightSelTextOccurrencesCheck.Checked := FOptions.HighlightSelTextOccurrences;
     OptionsForm.InspectorShowAllKnownDirectivesCheck.Checked := FOptions.InspectorShowAllKnownDirectives;
+    OptionsForm.InspectorFollowCaretCheck.Checked := FOptions.InspectorFollowCaret;
     OptionsForm.InspectorQuoteNewDirectiveValuesCheck.Checked := FOptions.InspectorQuoteNewDirectiveValues;
     OptionsForm.InspectorQuoteNewParameterValuesCheck.Checked := FOptions.InspectorQuoteNewParameterValues;
 
@@ -4391,6 +4410,7 @@ begin
     FOptions.HighlightWordAtCursorOccurrences := OptionsForm.HighlightWordAtCursorOccurrencesCheck.Checked;
     FOptions.HighlightSelTextOccurrences := OptionsForm.HighlightSelTextOccurrencesCheck.Checked;
     FOptions.InspectorShowAllKnownDirectives := OptionsForm.InspectorShowAllKnownDirectivesCheck.Checked;
+    FOptions.InspectorFollowCaret := OptionsForm.InspectorFollowCaretCheck.Checked;
     FOptions.InspectorQuoteNewDirectiveValues := OptionsForm.InspectorQuoteNewDirectiveValuesCheck.Checked;
     FOptions.InspectorQuoteNewParameterValues := OptionsForm.InspectorQuoteNewParameterValuesCheck.Checked;
 
@@ -4451,6 +4471,7 @@ begin
       Ini.WriteInteger('Options', 'EditorFontSize', FMainMemo.Font.Size);
       Ini.WriteInteger('Options', 'EditorFontCharset', FMainMemo.Font.Charset);
       Ini.WriteBool('Options', 'InspectorShowAllKnownDirectives', FOptions.InspectorShowAllKnownDirectives);
+      Ini.WriteBool('Options', 'InspectorFollowCaret', FOptions.InspectorFollowCaret);
       Ini.WriteBool('Options', 'InspectorQuoteNewParameterValues', FOptions.InspectorQuoteNewParameterValues);
       Ini.WriteBool('Options', 'InspectorQuoteNewDirectiveValues', FOptions.InspectorQuoteNewDirectiveValues);
     finally

Projects/Src/IDE.OptionsForm.pas
@@ -69,6 +69,7 @@ type
     LanguageComboBox: TComboBox;
     GroupBox2: TNewGroupBox;
     InspectorShowAllKnownDirectivesCheck: TCheckBox;
+    InspectorFollowCaretCheck: TCheckBox;
     InspectorQuoteNewDirectiveValuesCheck: TCheckBox;
     InspectorQuoteNewParameterValuesCheck: TCheckBox;
     procedure AssocButtonClick(Sender: TObject);

commit 1a803aabd37a1874eb9fa18394c0afc18c19b578

IDE.OptionsForm: Fix/cleanup alignment. Issue caused by e006b99ed and the akBottom anchors, remove latter.

Projects/Src/IDE.OptionsForm.dfm
@@ -237,8 +237,7 @@ object OptionsForm: TOptionsForm
     Left = 340
     Top = 8
     Width = 321
-    Height = 425
-    Anchors = [akLeft, akTop, akBottom]
+    Height = 405
     Caption = ' Editor '
     TabOrder = 2
     object AutoAutoCompleteCheck: TCheckBox
@@ -315,19 +314,18 @@ object OptionsForm: TOptionsForm
     end
     object Label1: TNewStaticText
       Left = 8
-      Top = 359
+      Top = 339
       Width = 25
       Height = 14
-      Anchors = [akLeft, akBottom]
       Caption = 'Font:'
       TabOrder = 16
     end
     object FontPanel: TPanel
       Left = 104
-      Top = 350
+      Top = 330
       Width = 129
       Height = 32
-      Anchors = [akLeft, akRight, akBottom]
+      Anchors = [akLeft, akTop, akRight]
       BevelKind = bkFlat
       BevelOuter = bvNone
       Caption = 'AaBbXxZz'
@@ -335,30 +333,29 @@ object OptionsForm: TOptionsForm
     end
     object ChangeFontButton: TButton
       Left = 240
-      Top = 355
+      Top = 335
       Width = 73
       Height = 23
-      Anchors = [akRight, akBottom]
+      Anchors = [akTop, akRight]
       Caption = '&Change...'
       TabOrder = 18
       OnClick = ChangeFontButtonClick
     end
     object Label2: TNewStaticText
       Left = 8
-      Top = 393
+      Top = 373
       Width = 54
       Height = 14
-      Anchors = [akLeft, akBottom]
       Caption = '&Tab Width:'
       FocusControl = TabWidthEdit
       TabOrder = 19
     end
     object TabWidthEdit: TEdit
       Left = 104
-      Top = 390
+      Top = 370
       Width = 50
       Height = 21
-      Anchors = [akLeft, akRight, akBottom]
+      Anchors = [akLeft, akTop, akRight]
       TabOrder = 20
       OnChange = TabWidthEditChange
     end
@@ -373,21 +370,20 @@ object OptionsForm: TOptionsForm
     end
     object Label4: TNewStaticText
       Left = 8
-      Top = 325
+      Top = 305
       Width = 37
       Height = 14
-      Anchors = [akLeft, akBottom]
       Caption = 'T&heme:'
       FocusControl = ThemeComboBox
       TabOrder = 14
     end
     object ThemeComboBox: TComboBox
       Left = 104
-      Top = 321
+      Top = 301
       Width = 209
       Height = 21
       Style = csDropDownList
-      Anchors = [akLeft, akRight, akBottom]
+      Anchors = [akLeft, akTop, akRight]
       TabOrder = 15
     end
     object HighlightSelTextOccurrencesCheck: TCheckBox
@@ -410,29 +406,28 @@ object OptionsForm: TOptionsForm
     end
     object Label5: TNewStaticText
       Left = 8
-      Top = 296
+      Top = 276
       Width = 27
       Height = 14
-      Anchors = [akLeft, akBottom]
       Caption = 'Ke&ys:'
       FocusControl = MemoKeyMappingComboBox
       TabOrder = 12
     end
     object MemoKeyMappingComboBox: TComboBox
       Left = 104
-      Top = 292
+      Top = 272
       Width = 209
       Height = 21
       Style = csDropDownList
-      Anchors = [akLeft, akRight, akBottom]
+      Anchors = [akLeft, akTop, akRight]
       TabOrder = 13
     end
     object ShowWhiteSpaceCheck: TCheckBox
       Left = 160
-      Top = 392
+      Top = 372
       Width = 153
       Height = 17
-      Anchors = [akRight, akBottom]
+      Anchors = [akTop, akRight]
       Caption = 'Show whitespace'
       TabOrder = 21
     end