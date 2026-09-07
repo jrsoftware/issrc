e13a2c6f19816169119aa0333c7ca17574164123
Jordan Russell <jr-github@jrsoftware.org>
2026-09-07 04:23:27 -0500

    Setup.MainFunc: Log initial RedirectionGuard mode.
    The initial mode is inherited from the parent process.

--- Projects/Src/Setup.MainFunc.pas
+++ Projects/Src/Setup.MainFunc.pas
@@ -2455,31 +2455,62 @@ var
 procedure RedirectionGuardConfigure(const AEnable: Boolean);
 const
   ProcessRedirectionTrustPolicy = TProcessMitigationPolicy(16);
+  EnforceRedirectionTrust = 1;
+  AuditRedirectionTrust = 2;
 var
+  GetProcessMitigationPolicyFunc: function(hProcess: THandle;
+    MitigationPolicy: TProcessMitigationPolicy; lpBuffer: Pointer;
+    dwLength: SIZE_T): BOOL; stdcall;
   SetProcessMitigationPolicyFunc: function(MitigationPolicy: TProcessMitigationPolicy;
     lpBuffer: PVOID; dwLength: SIZE_T): BOOL; stdcall;
+
+  function GetCurrentMode: String;
+  begin
+    var CurFlags: DWORD;
+    if GetProcessMitigationPolicyFunc(GetCurrentProcess,
+       ProcessRedirectionTrustPolicy, @CurFlags, SizeOf(CurFlags)) then begin
+      case CurFlags of
+        0: Result := 'Disabled';
+        EnforceRedirectionTrust: Result := 'Enforce';
+        AuditRedirectionTrust: Result := 'Audit';
+      else
+        Result := Format('Unknown (%u)', [CurFlags]);
+      end;
+    end else begin
+      const ErrorCode = GetLastError;
+      Result := Format('(GetProcessMitigationPolicy failed with error code %u)',
+        [ErrorCode]);
+    end;
+  end;
+
 begin
-  var Status: String;
+  const KernelModule = GetModuleHandle(kernel32);
+  GetProcessMitigationPolicyFunc := GetProcAddress(KernelModule,
+    PAnsiChar('GetProcessMitigationPolicy'));
+  SetProcessMitigationPolicyFunc := GetProcAddress(KernelModule,
+    PAnsiChar('SetProcessMitigationPolicy'));
+  if not Assigned(GetProcessMitigationPolicyFunc) or
+     not Assigned(SetProcessMitigationPolicyFunc) then begin
+    Log('RedirectionGuard: Get/SetProcessMitigationPolicy functions unavailable.');
+    Exit;
+  end;
+
+  LogFmt('RedirectionGuard: Initial mode for current process: %s',
+    [GetCurrentMode]);
 
   if AEnable then begin
-    SetProcessMitigationPolicyFunc := GetProcAddress(GetModuleHandle(kernel32),
-      PAnsiChar('SetProcessMitigationPolicy'));
-    if Assigned(SetProcessMitigationPolicyFunc) then begin
-      const Flags: DWORD = 1;  { = EnforceRedirectionTrust bit set }
-      if SetProcessMitigationPolicyFunc(ProcessRedirectionTrustPolicy, @Flags, SizeOf(Flags)) then begin
-        IsRedirectionGuardEnabled := True;
-        Status := 'Enabled in enforcing mode'
-      end else begin
-        const ErrorCode = GetLastError;
-        Status := Format('Could not enable (SetProcessMitigationPolicy failed with error code %u)',
-          [ErrorCode]);
-      end;
-    end else
-      Status := 'Could not enable (SetProcessMitigationPolicy unavailable)';
+    const Flags: DWORD = EnforceRedirectionTrust;
+    if SetProcessMitigationPolicyFunc(ProcessRedirectionTrustPolicy, @Flags, SizeOf(Flags)) then begin
+      IsRedirectionGuardEnabled := True;
+      Log('RedirectionGuard: Mode changed to Enforce.');
+    end else begin
+      const ErrorCode = GetLastError;
+      LogFmt('RedirectionGuard: Could not change mode to Enforce. ' +
+        '(SetProcessMitigationPolicy failed with error code %u)',
+        [ErrorCode]);
+    end;
   end else
-    Status := 'Not enabling';
-
-  LogFmt('RedirectionGuard status for current process: %s', [Status]);
+    Log('RedirectionGuard: Not changing mode.');
 end;
 
 function RedirectionGuardEnabled: Boolean;
